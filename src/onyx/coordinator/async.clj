94e923352f7f7d03d546ada41839dd13b0b726c0
Michael Drogalis <mjd3089@rit.edu>
2013-12-27 20:51:12 -0500

    More channels.

--- src/onyx/coordinator/async.clj
+++ src/onyx/coordinator/async.clj
@@ -1,10 +1,12 @@
 (ns onyx.coordinator.async
-  (:require [clojure.core.async :refer [chan thread >!! <!!]]
+  (:require [clojure.core.async :refer [chan thread timeout >!! <!!]]
             [onyx.coordinator.extensions :as extensions]))
+
+(def eviction-delay 5000)
 
 (def planning-ch (chan 1000))
 
-(def new-peer-ch (chan 1000))
+(def born-peer-ch (chan 1000))
 
 (def dead-peer-ch (chan 1000))
 
@@ -16,6 +18,17 @@
 
 (def completion-ch (chan 1000))
 
+(defn mark-peer-birth [log sync peer]
+  (extensions/await-death sync dead-peer-ch)
+  (extensions/mark-peer-born log peer))
+
+(defn mark-peer-death [log peer]
+  (extensions/mark-peer-dead log peer))
+
+(defn plan-job [log job]
+  (comment "Planning here.")
+  (extensions/plan-job log job))
+
 (defn acknowledge-task [log task]
   (extensions/ack log task))
 
@@ -23,10 +36,40 @@
   (extensions/delete sync task)
   (extensions/evict log task))
 
+(defn offer-task [log sync]
+  (when (extensions/next-task log)
+    (extensions/create-ack-node sync)
+    (extensions/create-completion-node sync)
+    (extensions/await-ack sync ack-ch)
+    (extensions/await-completion sync completion-ch)
+    (extensions/mark-offered log)
+    (extensions/write-payload sync)))
+
 (defn complete-task [log sync queue task]
   (extensions/delete sync task)
   (extensions/complete log task)
   (extensions/cap-queue queue task))
+
+(defn born-peer-ch-loop [log sync]
+  (loop []
+    (let [peer (<!! born-peer-ch)]
+      (mark-peer-birth log sync peer)
+      (>!! offer-ch peer)
+      (recur))))
+
+(defn dead-peer-ch-loop [log]
+  (loop []
+    (let [peer (<!! dead-peer-ch)]
+      (mark-peer-death log peer)
+      (>!! evict-ch peer)
+      (recur))))
+
+(defn planning-ch-loop [log]
+  (loop []
+    (let [job (<!! planning-ch)]
+      (plan-job log job)
+      (<!! offer-ch job)
+      (recur))))
 
 (defn ack-ch-loop [log]
   (loop []
@@ -41,6 +84,14 @@
       (>!! offer-ch task)
       (recur))))
 
+(defn offer-ch-loop [log sync]
+  (loop []
+    (let [event (<!! offer-ch)]
+      (when (offer-task log sync)
+        (thread (<!! (timeout eviction-delay))
+                (>!! evict-ch nil)))
+      (recur))))
+
 (defn completion-ch-loop [log sync queue]
   (loop []
     (let [task (<!! completion-ch)]
@@ -49,7 +100,11 @@
       (recur))))
 
 (defn ch-graph [log sync queue]
+  (thread (born-peer-ch-loop log sync))
+  (thread (dead-peer-ch-loop log))
+  (thread (planning-ch-loop))
   (thread (ack-ch-loop log))
   (thread (evict-ch-loop log sync))
+  (thread (offer-ch-loop log sync))
   (thread (completion-ch-loop log sync queue)))
 
--- src/onyx/coordinator/extensions.clj
+++ src/onyx/coordinator/extensions.clj
@@ -1,12 +1,34 @@
 (ns onyx.coordinator.extensions)
 
+(defmulti mark-peer-born (fn [log peer] log))
+
+(defmulti mark-peer-dead (fn [log peer] log))
+
+(defmulti mark-offered (fn [log] log))
+
+(defmulti plan-job (fn [log job] log))
+
 (defmulti ack (fn [log task] log))
 
 (defmulti evict (fn [log task] log))
 
 (defmulti complete (fn [log task] log))
 
+(defmulti next-task (fn [log] log))
+
 (defmulti delete (fn [sync task] sync))
+
+(defmulti create-ack-node (fn [sync] sync))
+
+(defmulti create-completion-node (fn [sync] sync))
+
+(defmulti await-ack (fn [sync node] sync))
+
+(defmulti await-completion (fn [sync node] sync))
+
+(defmulti await-death (fn [sync peer] sync))
+
+(defmulti write-payload (fn [sync] sync))
 
 (defmulti cap-queue (fn [queue task] queue))
 
