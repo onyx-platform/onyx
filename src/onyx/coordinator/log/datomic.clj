44b442a23ec2062fe36c4cd54e88fe0c0abe5458
Michael Drogalis <mjd3089@rit.edu>
2013-12-28 12:03:37 -0500

    Set schema.

--- src/onyx/coordinator/log/datomic.clj
+++ src/onyx/coordinator/log/datomic.clj
@@ -1,5 +1,20 @@
 (ns onyx.coordinator.log.datomic
-  (:require [onyx.coordinator.extensions :as extensions]))
+  (:require [datomic.api :as d]
+            [onyx.coordinator.extensions :as extensions]))
+
+(defn log-schema []
+  (let [resource (clojure.java.io/resource "datomic-schema.edn")]
+    (read-string (slurp resource))))
+
+(defn schema-set? [conn]
+  (seq (d/q '[:find ?e :where [?e :db/ident :peer/place]] (d/db conn))))
+
+(defn start-datomic! [uri schema]
+  (d/create-database uri)
+  (let [conn (d/connect uri)]
+    (when-not (schema-set? conn)
+      @(d/transact conn schema))
+    conn))
 
 (defmethod extensions/mark-peer-born :datomic
   [log peer])
