47145dbcb993f2fc3fa0213e8d11c074e47c26c5
Michael Drogalis <mjd3089@rit.edu>
2014-01-14 19:25:02 -0500

    More test

--- test/onyx/coordinator/multi_peer_test.clj
+++ test/onyx/coordinator/multi_peer_test.clj
@@ -1,6 +1,6 @@
 (ns onyx.coordinator.multi-peer-test
   (:require [clojure.test :refer :all]
-            [clojure.core.async :refer [chan tap >!! <!!]]
+            [clojure.core.async :refer [chan tap alts!! >!! <!!]]
             [com.stuartsierra.component :as component]
             [datomic.api :as d]
             [onyx.coordinator.async :as async]
@@ -16,13 +16,16 @@
       (let [peer-node-a (extensions/create sync :peer)
             peer-node-b (extensions/create sync :peer)
 
-            payload-node-a (extensions/create sync :payload)
-            payload-node-b (extensions/create sync :payload)
+            payload-node-a-1 (extensions/create sync :payload)
+            payload-node-b-1 (extensions/create sync :payload)
+
+            payload-node-a-2 (extensions/create sync :payload)
+            payload-node-b-2 (extensions/create sync :payload)
 
             sync-spy-a (chan 1)
             sync-spy-b (chan 1)
             ack-ch-spy (chan 2)
-            offer-ch-spy (chan 1)
+            offer-ch-spy (chan 10)
             status-spy (chan 2)
             
             catalog [{:onyx/name :in
@@ -45,11 +48,11 @@
         (tap (:ack-mult coordinator) ack-ch-spy)
         (tap (:offer-mult coordinator) offer-ch-spy)
 
-        (extensions/write-place sync peer-node-a payload-node-a)
-        (extensions/on-change sync payload-node-a #(>!! sync-spy-a %))
+        (extensions/write-place sync peer-node-a payload-node-a-1)
+        (extensions/on-change sync payload-node-a-1 #(>!! sync-spy-a %))
 
-        (extensions/write-place sync peer-node-b payload-node-b)
-        (extensions/on-change sync payload-node-b #(>!! sync-spy-b %))
+        (extensions/write-place sync peer-node-b payload-node-b-1)
+        (extensions/on-change sync payload-node-b-1 #(>!! sync-spy-b %))
 
         (>!! (:born-peer-ch-head coordinator) peer-node-a)
         (>!! (:born-peer-ch-head coordinator) peer-node-b)
@@ -59,12 +62,13 @@
 
         (>!! (:planning-ch-head coordinator) {:catalog catalog :workflow workflow})
 
+        (<!! offer-ch-spy)
         (<!! sync-spy-a)
         (<!! sync-spy-b)
 
         (testing "Both payloads are received"
-          (let [payload-a (extensions/read-place sync payload-node-a)
-                payload-b (extensions/read-place sync payload-node-b)]
+          (let [payload-a (extensions/read-place sync payload-node-a-1)
+                payload-b (extensions/read-place sync payload-node-b-1)]
             (is (not (nil? payload-a)))
             (is (not (nil? payload-b)))
             (is (not= payload-a payload-b))
@@ -81,10 +85,35 @@
             (<!! status-spy)
             (<!! status-spy)
 
+            (extensions/write-place sync peer-node-a payload-node-a-2)
+            (extensions/on-change sync payload-node-a-2 #(>!! sync-spy-a %))
+
+            (extensions/write-place sync peer-node-b payload-node-b-2)
+            (extensions/on-change sync payload-node-b-2 #(>!! sync-spy-b %))
+
             (extensions/touch-place sync (:completion (:nodes payload-a)))
             (extensions/touch-place sync (:completion (:nodes payload-b)))
-            
-            ))))
+
+            (<!! offer-ch-spy)
+            (<!! offer-ch-spy)
+
+            (let [[v ch] (alts!! [sync-spy-a sync-spy-b])
+                  nodes (:nodes (extensions/read-place sync (:path v)))]
+              (extensions/on-change sync (:status nodes) #(>!! status-spy %))
+              (extensions/touch-place sync (:ack nodes))
+
+              (<!! ack-ch-spy)
+              (<!! status-spy)
+
+              (extensions/touch-place sync (:completion nodes))
+              (<!! offer-ch-spy))
+
+            (testing "All tasks are complete"
+              (let [db (d/db (:conn log))
+                    query '[:find (count ?task) :where
+                            [?task :task/complete? true]]
+                    result (ffirst (d/q query db))]
+                (is (= result 3))))))))
     
     {:eviction-delay 50000}))
 
