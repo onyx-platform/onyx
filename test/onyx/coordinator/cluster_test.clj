b70d96f75b843af933d9026a2082326f134eb79a
Michael Drogalis <mjd3089@rit.edu>
2014-01-22 17:49:36 -0500

    Axe Simulant for now.

--- test/onyx/coordinator/cluster_test.clj
+++ test/onyx/coordinator/cluster_test.clj
@@ -1,40 +1,12 @@
-(ns onyx.coordinator.coordinator-simulant-tests
+(ns onyx.coordinator.cluster-test
   (:require [clojure.test :refer :all]
             [clojure.core.async :refer [chan <!! >!! <! >! tap go]]
             [clojure.data.generators :as gen]
             [com.stuartsierra.component :as component]
-            [simulant.sim :as sim]
-            [simulant.util :refer [tx-ent e hours->msec getx] :as u]
             [datomic.api :as d]
             [onyx.system :as s]
             [onyx.coordinator.extensions :as extensions]
             [onyx.coordinator.log.datomic :as datomic]))
-
-(defn minutes->msec [m]
-  (long (* m 60 1000)))
-
-(defn reset-conn
-  ([]
-     (reset-conn (str "datomic:mem://" (d/squuid))))
-  ([uri]
-     (d/delete-database uri)
-     (d/create-database uri)
-     (d/connect uri)))
-
-(defn load-schema
-  [conn resource]
-  (let [m (-> resource clojure.java.io/resource slurp read-string)]
-    (doseq [v (vals m)]
-      (doseq [tx v]
-        (d/transact conn tx)))))
-
-(def sim-uri (str "datomic:mem://" (d/squuid)))
-
-(def sim-conn (reset-conn sim-uri))
-
-(load-schema sim-conn "simulant/schema.edn")
-
-(load-schema sim-conn "simulant/coordinator-sim.edn")
 
 (def system (s/onyx-system {:sync :zookeeper :queue :hornetq :eviction-delay 500000}))
 
@@ -107,78 +79,7 @@
                   (recur next-payload)))))
           (catch Exception e (prn e))))))
 
-(defn create-test
-  [conn model test]
-  (u/require-keys test :db/id :test/duration)
-  (-> @(d/transact conn [(assoc test
-                           :test/type :test.type/stubbed-peer
-                           :model/_tests (e model))])
-      (tx-ent (:db/id test))))
-
-(defn create-peers [conn test]
-  (let [model (-> test :model/_tests u/solo)
-        ids (repeatedly (:model/peer-count model) #(d/tempid :test))
-        txresult (->> ids
-                      (map (fn [id] {:db/id id
-                                    :agent/type :agent.type/stable-peer
-                                    :test/_agents (e test)}))
-                      (d/transact conn))]
-    (u/tx-entids @txresult ids)))
-
-(defn generate-peer-executions [test peer]
-  (let [model (-> test :model/_tests u/solo)
-        limit (:test/duration test)]
-    []))
-
-(defn generate-all-executions [test peers]
-  (mapcat (partial generate-peer-executions test) peers))
-
-(defmethod sim/create-test :model.type/stubbed-peer
-  [conn model test]
-  (let [test (create-test conn model test)
-        peers (create-peers conn test)]
-    (u/transact-batch conn (generate-all-executions test peers) 1000)
-    (d/entity (d/db conn) (e test))))
-
-(defmethod sim/create-sim :test.type/stubbed-peer
-  [sim-conn test sim]
-  (let [model (-> test :model/_tests u/solo)]
-    (-> @(d/transact sim-conn (sim/construct-basic-sim test sim))
-        (tx-ent (:db/id sim)))))
-
-(def model-id (d/tempid :model))
-
-(def coordinator-model-data
-  [{:db/id model-id
-    :model/type :model.type/stubbed-peer
-    :model/peer-count 10}])
-
-(def coordinator-model
-  (-> @(d/transact sim-conn coordinator-model-data)
-      (tx-ent model-id)))
-
-(def coordinator-test
-  (sim/create-test sim-conn coordinator-model
-                   {:db/id (d/tempid :test)
-                    :test/duration (hours->msec 1)}))
-
-(def coordinator-sim
-  (sim/create-sim sim-conn coordinator-test
-                  {:db/id (d/tempid :sim)
-                   :sim/systemURI (str "datomic:mem://" (d/squuid))
-                   :sim/processCount 10}))
-
-(def sim-clock (sim/create-fixed-clock sim-conn coordinator-sim {:clock/multiplier 10000}))
-
 (start-peers! peers)
-
-(def pruns
-  (->> #(sim/run-sim-process sim-uri (:db/id coordinator-sim))
-       (repeatedly (:sim/processCount coordinator-sim))
-       (into [])))
-
-(time
- (mapv (fn [prun] @(:runner prun)) pruns))
 
 (testing "It must complete in under 10 seconds"
   (Thread/sleep 10000))
