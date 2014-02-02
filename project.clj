4df4afb0b9cc0bfd73f82562a605fb98238c97a0
Michael Drogalis <mjd3089@rit.edu>
2014-02-01 21:26:59 -0500

    Adds 2.4.0 HornetQ.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/data.generators "0.1.2"]
+                 [org.hornetq/hornetq-core-client "2.4.0.Final"]
                  [com.stuartsierra/component "0.2.1"]
                  [com.datomic/datomic-free "0.9.4384"]
                  [com.datomic/simulant "0.1.6"]
