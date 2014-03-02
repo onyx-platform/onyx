77b5ed4d4aa28fa47b0bf8de7ce1e468fcd09d7e
Michael Drogalis <mjd3089@rit.edu>
2014-03-01 22:30:25 -0500

    Upgrade to Clojure 1.6.0-beta2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0-beta2"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/data.generators "0.1.2"]
                  [org.hornetq/hornetq-core-client "2.4.0.Final"]
