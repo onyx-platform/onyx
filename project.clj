271634e16f1fdc18962736cc6e8e73fb1aee31f8
Michael Drogalis <mjd3089@rit.edu>
2014-03-30 21:03:23 -0400

    Namespace the project under com.mdrogalis

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx "0.1.0-SNAPSHOT"
+(defproject com.mdrogalis/onyx "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
