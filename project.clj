50b4fd13a080d6d01858646eeef2dd9204cd2be0
Michael Drogalis <mjd3089@rit.edu>
2014-04-07 20:51:25 -0400

    Update description and url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.mdrogalis/onyx "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Distributed data processing"
+  :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
