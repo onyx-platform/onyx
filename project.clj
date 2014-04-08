701e4a76ef06eb8e86bf7a8621049cb6168a8b75
Michael Drogalis <mjd3089@rit.edu>
2014-04-07 20:55:38 -0400

    Bump to 0.1.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.mdrogalis/onyx "0.1.0"
+(defproject com.mdrogalis/onyx "0.1.1-SNAPSHOT"
   :description "Distributed data processing"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
