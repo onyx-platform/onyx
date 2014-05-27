8696c5fbea79acf93835b45395f7c721e7803a72
Michael Drogalis <mjd3089@rit.edu>
2014-05-27 18:37:52 -0400

    Let Onyx coordinator & peer take a config for logging.

--- src/onyx/logging_configuration.clj
+++ src/onyx/logging_configuration.clj
@@ -2,13 +2,16 @@
   (:require [com.stuartsierra.component :as component]
             [taoensso.timbre :refer [info] :as timbre]))
 
-(defrecord LoggingConfiguration [file]
+(defrecord LoggingConfiguration [file config]
   component/Lifecycle
 
   (start [component]
-    (timbre/set-config! [:appenders :standard-out :enabled?] false)
-    (timbre/set-config! [:appenders :spit :enabled?] true)
-    (timbre/set-config! [:shared-appender-config :spit-filename] file)
+    (if config
+      (timbre/set-config! config)
+      (do
+        (timbre/set-config! [:appenders :standard-out :enabled?] false)
+        (timbre/set-config! [:appenders :spit :enabled?] true)
+        (timbre/set-config! [:shared-appender-config :spit-filename] file)))
 
     (info "Starting Logging Configuration")
 
@@ -18,6 +21,7 @@
     (info "Stopping Logging Configuration")
     component))
 
-(defn logging-configuration [file]
-  (map->LoggingConfiguration {:file (or file "onyx.log")}))
+(defn logging-configuration
+  ([file] (logging-configuration file nil))
+  ([file config] (map->LoggingConfiguration {:file (or file "onyx.log") :config config})))
 
