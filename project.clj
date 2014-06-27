5ec4bf7201fe7bc4685e11c41e299646deb7a61b
Michael Drogalis <mjd3089@rit.edu>
2014-06-26 17:47:00 -0700

    0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.mdrogalis/onyx "0.2.0-SNAPSHOT"
+(defproject com.mdrogalis/onyx "0.2.0"
   :description "Distributed data processing"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
