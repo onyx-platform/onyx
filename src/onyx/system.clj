18b64e3ad453f03a8b2576fd53b64fdd2988694c
Michael Drogalis <mjd3089@rit.edu>
2014-07-25 07:31:13 -0700

    Throw better exception.

--- src/onyx/system.clj
+++ src/onyx/system.clj
@@ -10,19 +10,38 @@
 
 (def peer-components [:logging-config :sync :queue :peer])
 
+(defn rethrow-component [f]
+  (try
+    (f)
+    (catch Exception e
+      (throw (.getCause e)))))
+
 (defrecord OnyxCoordinator [logging-config sync queue]
   component/Lifecycle
   (start [this]
-    (component/start-system this coordinator-components))
+    (rethrow-component
+     #(component/start-system this coordinator-components)))
   (stop [this]
-    (component/stop-system this coordinator-components)))
+    (rethrow-component
+     #(component/stop-system this coordinator-components))))
+
+(defrecord OnyxCoordinatorConnection [logging-config sync]
+  component/Lifecycle
+  (start [this]
+    (rethrow-component
+     #(component/start-system this connection-components)))
+  (stop [this]
+    (rethrow-component
+     #(component/stop-system this connection-components))))
 
 (defrecord OnyxPeer [logging-config sync queue]
   component/Lifecycle
   (start [this]
-    (component/start-system this peer-components))
+    (rethrow-component
+     #(component/start-system this peer-components)))
   (stop [this]
-    (component/stop-system this peer-components)))
+    (rethrow-component
+     #(component/stop-system this peer-components))))
 
 (defn onyx-coordinator
   [opts]
