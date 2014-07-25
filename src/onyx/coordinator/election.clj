6fef26d8104bfb6af55ce608c3db3ff12cdb7e0d
Michael Drogalis <mjd3089@rit.edu>
2014-07-24 20:48:58 -0700

    Start test for HA coordination.

--- src/onyx/coordinator/election.clj
+++ src/onyx/coordinator/election.clj
@@ -3,24 +3,21 @@
               [onyx.extensions :as extensions]))
 
 (defn block-until-leader! [sync content]
-  (try
-    (prn "Coordinator announced")
-    (let [node (:node (extensions/create sync :election content))]
-      (prn "Node is " node)
-      (loop []
-        (let [ch (chan)
-              predecessor (extensions/previous-node sync :election node)]
-          (prn "Previous node is " predecessor)
-          (when predecessor
-            (extensions/on-delete sync predecessor #(>!! ch))
+  (prn "Coordinator announced")
+  (let [node (:node (extensions/create sync :election content))]
+    (prn "Node is " node)
+    (loop []
+      (let [ch (chan)
+            predecessor (extensions/previous-node sync :election node)]
+        (prn "Previous node is " predecessor)
+        (when predecessor
+          (extensions/on-delete sync predecessor #(>!! ch))
 
-            (prn "Blocking")
-            (<!! ch)
-            (prn "Unblocking")
-            (let [children (extensions/children sync :election)]
-              (prn "Children are " children)
-              (when-not (extensions/smallest? sync :election node)
-                (recur)))))))
-    (catch Exception e
-      (.printStackTrace e))))
+          (prn "Blocking")
+          (<!! ch)
+          (prn "Unblocking")
+          (let [children (extensions/children sync :election)]
+            (prn "Children are " children)
+            (when-not (extensions/smallest? sync :election node)
+              (recur))))))))
 
---
+++ test/onyx/coordinator/coordinator_ha_test.clj
@@ -0,0 +1,110 @@
+(ns onyx.coordinator.coordinator-ha-test
+  (:require [midje.sweet :refer :all]
+            [onyx.queue.hornetq-utils :as hq-util]
+            [onyx.coordinator.distributed :as d]
+            [onyx.api]))
+
+(def config (read-string (slurp (clojure.java.io/resource "test-config.edn"))))
+
+(def n-messages 15000)
+
+(def batch-size 1320)
+
+(def echo 1000)
+
+(def id (str (java.util.UUID/randomUUID)))
+
+(def in-queue (str (java.util.UUID/randomUUID)))
+
+(def out-queue (str (java.util.UUID/randomUUID)))
+
+(def onyx-port (+ 10000 (rand-int 10000)))
+
+(def hq-config {"host" (:host (:non-clustered (:hornetq config)))
+                "port" (:port (:non-clustered (:hornetq config)))})
+
+(def coord-opts
+  {:hornetq/mode :udp
+   :hornetq/server? true
+   :hornetq.udp/cluster-name (:cluster-name (:hornetq config))
+   :hornetq.udp/group-address (:group-address (:hornetq config))
+   :hornetq.udp/group-port (:group-port (:hornetq config))
+   :hornetq.udp/refresh-timeout (:refresh-timeout (:hornetq config))
+   :hornetq.udp/discovery-timeout (:discovery-timeout (:hornetq config))
+   :hornetq.server/type :embedded
+   :hornetq.embedded/config (:configs (:hornetq config))
+   :zookeeper/address (:address (:zookeeper config))
+   :zookeeper/server? true
+   :zookeeper.server/port (:spawn-port (:zookeeper config))
+   :onyx/id id
+   :onyx.coordinator/host "localhost"
+   :onyx.coordinator/port onyx-port
+   :onyx.coordinator/revoke-delay 5000})
+
+(def peer-opts
+  {:hornetq/mode :udp
+   :hornetq.udp/cluster-name (:cluster-name (:hornetq config))
+   :hornetq.udp/group-address (:group-address (:hornetq config))
+   :hornetq.udp/group-port (:group-port (:hornetq config))
+   :hornetq.udp/refresh-timeout (:refresh-timeout (:hornetq config))
+   :hornetq.udp/discovery-timeout (:discovery-timeout (:hornetq config))
+   :zookeeper/address (:address (:zookeeper config))
+   :onyx/id id})
+
+(def onyx-server (d/start-distributed-coordinator coord-opts))
+
+(def conn (onyx.api/connect :distributed coord-opts))
+
+(hq-util/create-queue! hq-config in-queue)
+(hq-util/create-queue! hq-config out-queue)
+
+(hq-util/write-and-cap! hq-config in-queue (map (fn [x] {:n x}) (range n-messages)) echo)
+
+(defn my-inc [{:keys [n] :as segment}]
+  (assoc segment :n (inc n)))
+
+(def catalog
+  [{:onyx/name :in
+    :onyx/ident :hornetq/read-segments
+    :onyx/type :input
+    :onyx/medium :hornetq
+    :onyx/consumption :concurrent
+    :hornetq/queue-name in-queue
+    :hornetq/host (:host (:non-clustered (:hornetq config)))
+    :hornetq/port (:port (:non-clustered (:hornetq config)))
+    :onyx/batch-size batch-size}
+   
+   {:onyx/name :inc
+    :onyx/fn :onyx.coordinator.coordinator-ha-test/my-inc
+    :onyx/type :transformer
+    :onyx/consumption :concurrent
+    :onyx/batch-size batch-size}
+   
+   {:onyx/name :out
+    :onyx/ident :hornetq/write-segments
+    :onyx/type :output
+    :onyx/medium :hornetq
+    :onyx/consumption :concurrent
+    :hornetq/queue-name out-queue
+    :hornetq/host (:host (:non-clustered (:hornetq config)))
+    :hornetq/port (:port (:non-clustered (:hornetq config)))
+    :onyx/batch-size batch-size}])
+
+(def workflow {:in {:inc :out}})
+
+(def v-peers (onyx.api/start-peers conn 1 peer-opts))
+
+(onyx.api/submit-job conn {:catalog catalog :workflow workflow})
+
+(def results (hq-util/consume-queue! hq-config out-queue echo))
+
+(doseq [v-peer v-peers]
+  ((:shutdown-fn v-peer)))
+
+(onyx.api/shutdown conn)
+(d/stop-distributed-coordinator onyx-server)
+
+(let [expected (set (map (fn [x] {:n (inc x)}) (range n-messages)))]
+  (fact (set (butlast results)) => expected)
+  (fact (last results) => :done))
+
