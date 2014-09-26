ac4e9e48281632824a7b5e8b82f38011db5c8eaa
Michael Drogalis <mjd3089@rit.edu>
2014-09-25 20:02:17 -0700

    Move Datomic and Simulant into dev deps.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/data.generators "0.1.2"]
                  [org.clojure/data.fressian "0.2.0"
                   :exclusions [org.fressian/fressian]]
@@ -14,16 +13,17 @@
                  [org.hornetq/hornetq-server "2.4.0.Final"]
                  [org.apache.curator/curator-test "2.6.0"]
                  [com.stuartsierra/component "0.2.1"]
-                 [com.datomic/datomic-free "0.9.4755"
-                  :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [com.datomic/simulant "0.1.6"]
                  [com.taoensso/timbre "3.0.1"]
                  [javax.servlet/servlet-api "2.5"]
                  [zookeeper-clj "0.9.1" :exclusions [commons-codec]]
                  [clj-http "0.9.1"]
                  [ring "1.2.2" :exclusions [joda-time]]
                  [dire "0.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [com.datomic/datomic-free "0.9.4755"
+                                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                                  [com.datomic/simulant "0.1.6"]
+                                  [org.clojure/tools.nrepl "0.2.3"]]
                    :plugins [[lein-midje "3.1.1"]
                              [codox "0.8.8"]]}
              :circle-ci {:jvm-opts ["-Xmx4g"]}}
