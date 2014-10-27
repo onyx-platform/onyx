a98e3da22625902649c697743b51e820cdbf4615
Michael Drogalis <mjd3089@rit.edu>
2014-10-27 08:36:34 -0700

    Use Fressian, Datomc is only available in dev.

--- project.clj
+++ project.clj
@@ -7,8 +7,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/data.generators "0.1.2"]
-                 [org.clojure/data.fressian "0.2.0"
-                  :exclusions [org.fressian/fressian]]
+                 [org.clojure/data.fressian "0.2.0"]
                  [org.hornetq/hornetq-commons "2.4.0.Final"]
                  [org.hornetq/hornetq-core-client "2.4.0.Final"]
                  [org.hornetq/hornetq-server "2.4.0.Final"]
@@ -24,7 +23,8 @@
                  [prismatic/schema "0.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [com.datomic/datomic-free "0.9.4755"
-                                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                                   :exclusions [com.fasterxml.jackson.core/jackson-core]
+                                   :exclusions [org.fressian/fressian]]
                                   [com.datomic/simulant "0.1.6"]
                                   [org.clojure/tools.nrepl "0.2.3"]]
                    :plugins [[lein-midje "3.1.1"]
