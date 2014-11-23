4d6d2522e1fdae6d61b35b63fd935c1c6f640b1c
Michael Drogalis <mjd3089@rit.edu>
2014-11-22 22:04:38 -0800

    Algorithm for peer shift.

--- src/onyx/log/commands/submit_job.clj
+++ src/onyx/log/commands/submit_job.clj
@@ -10,19 +10,34 @@
       (update-in [:jobs] conj (:id args))
       (update-in [:jobs] vec)
       (assoc-in [:task-schedulers (:id args)] (:task-scheduler args))
-      (assoc-in [:tasks (:id args)] (:tasks args))))
+      (assoc-in [:tasks (:id args)] (:tasks args))
+      (assoc-in [:allocations (:id args)] {})))
 
 (defmethod extensions/replica-diff :submit-job
   [{:keys [args]} old new]
   {:job (:id args)})
 
+(defn job->n-peers [replica]
+  (let [j (count (:jobs replica))
+        p (count (:peers replica))
+        min-peers (int (/ p j))
+        r (/ min-peers j)
+        n (numerator r)
+        max-peers (inc min-peers)]
+    (into {}
+          (map-indexed
+           (fn [i [job-id tasks]]
+             {job-id (if (< i n) max-peers min-peers)})
+           (:allocations replica)))))
+
 (defmethod extensions/reactions :submit-job
   [entry old new diff peer-args]
-  (if (and (= (:job-scheduler old) :onyx.job-scheduler/greedy)
-           (not (seq (:jobs old))))
-    [{:fn :volunteer-for-task
-      :args {:id (:id peer-args)}}]
-    []))
+  (cond (and (= (:job-scheduler old) :onyx.job-scheduler/greedy)
+             (not (seq (:jobs old))))
+        [{:fn :volunteer-for-task
+          :args {:id (:id peer-args)}}]
+        (= (:job-scheduler old) :onyx.job-scheduler/round-robin)
+        42))
 
 (defmethod extensions/fire-side-effects! :submit-job
   [entry old new diff state]
