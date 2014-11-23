b49280c25fce0655e0e07258a18484f15b89e9b1
Michael Drogalis <mjd3089@rit.edu>
2014-11-23 10:21:29 -0800

    Submit job reaction WIP.

--- src/onyx/log/commands/submit_job.clj
+++ src/onyx/log/commands/submit_job.clj
@@ -22,22 +22,36 @@
         p (count (:peers replica))
         min-peers (int (/ p j))
         r (/ min-peers j)
-        n (numerator r)
+        n (if (integer? r) 0 (numerator r))
         max-peers (inc min-peers)]
     (into {}
           (map-indexed
            (fn [i [job-id tasks]]
-             {job-id (if (< i n) max-peers min-peers)})
+             {job-id (if (<= i n) max-peers min-peers)})
            (:allocations replica)))))
+
+(defn peer->allocated-job [allocations id]
+  (get
+   (reduce-kv
+    (fn [all job tasks]
+      (->> tasks
+           (mapcat (fn [[t ps]] (map (fn [p] {p {:job job :task t}}) ps)))
+           (into {})
+           (merge all)))
+    {} allocations)
+   id))
 
 (defmethod extensions/reactions :submit-job
   [entry old new diff peer-args]
   (cond (and (= (:job-scheduler old) :onyx.job-scheduler/greedy)
              (not (seq (:jobs old))))
-        [{:fn :volunteer-for-task
-          :args {:id (:id peer-args)}}]
+        [{:fn :volunteer-for-task :args {:id (:id peer-args)}}]
         (= (:job-scheduler old) :onyx.job-scheduler/round-robin)
-        42))
+        (if-let [allocation (peer->allocated-job (:allocations new) (:id peer-args))]
+          (let [peer-counts (job->n-peers new)]
+            (prn "~>" peer-counts)
+            [{:fn :volunteer-for-task :args {:id (:id peer-args)}}])
+          [{:fn :volunteer-for-task :args {:id (:id peer-args)}}])))
 
 (defmethod extensions/fire-side-effects! :submit-job
   [entry old new diff state]
---
+++ test/onyx/log/two_job_test.clj
@@ -0,0 +1,60 @@
+(ns onyx.log.two-job-test
+  (:require [clojure.core.async :refer [chan >!! <!! close!]]
+            [com.stuartsierra.component :as component]
+            [onyx.system :refer [onyx-development-env]]
+            [onyx.log.entry :refer [create-log-entry]]
+            [onyx.extensions :as extensions]
+            [onyx.api :as api]
+            [midje.sweet :refer :all]
+            [zookeeper :as zk]))
+
+(def onyx-id (java.util.UUID/randomUUID))
+
+(def config (read-string (slurp (clojure.java.io/resource "test-config.edn"))))
+
+(def dev (onyx-development-env onyx-id (:env config)))
+
+(def env (component/start dev))
+
+(def peer-opts
+  {:inbox-capacity 1000
+   :outbox-capacity 1000
+   :job-scheduler :onyx.job-scheduler/round-robin})
+
+(def n-peers 5)
+
+(def v-peers (onyx.api/start-peers! onyx-id n-peers (:peer config) peer-opts))
+
+(onyx.api/submit-job (:log env)
+                     {:workflow [[:a :b] [:b :c]]
+                      :catalog []
+                      :task-scheduler :onyx.task-scheduler/round-robin})
+
+(Thread/sleep 500)
+
+(onyx.api/submit-job (:log env)
+                     {:workflow [[:d :e] [:e :f]]
+                      :catalog []
+                      :task-scheduler :onyx.task-scheduler/round-robin})
+
+(def ch (chan n-peers))
+
+(extensions/subscribe-to-log (:log env) 0 ch)
+
+(def replica
+  (loop [replica {:job-scheduler (:job-scheduler peer-opts)}]
+    (let [position (<!! ch)
+          entry (extensions/read-log-entry (:log env) position)
+          new-replica (extensions/apply-log-entry entry replica)]
+      (println)
+      (clojure.pprint/pprint entry)
+      (clojure.pprint/pprint new-replica)
+      (recur new-replica))))
+
+(doseq [v-peer v-peers]
+  (try
+    ((:shutdown-fn v-peer))
+    (catch Exception e (prn e))))
+
+(component/stop env)
+
