d75417f439a5388e81073f624f02eb3740c84207
Michael Drogalis <mjd3089@rit.edu>
2014-12-13 21:21:45 -0800

    Release peers that arent sealing.

--- src/onyx/log/commands/seal_task.clj
+++ src/onyx/log/commands/seal_task.clj
@@ -6,15 +6,16 @@
             [onyx.extensions :as extensions]))
 
 (defn should-seal? [replica args]
-  (let [n-peers (count (get-in replica [:allocations (:job args) (:task args)]))
-        status (common/task-status replica (:job args) (:task args))]
+  (let [status (common/task-status replica (:job args) (:task args))]
     (boolean
      (and (= (get status :active 0) 1)
           (= (get-in replica [:peer-state (:id args)]) :active)))))
 
 (defmethod extensions/apply-log-entry :seal-task
   [{:keys [args]} replica]
-  replica)
+  (if-not (should-seal? replica args)
+    (assoc-in replica [:peer-state (:id args)] :idle)
+    replica))
 
 (defmethod extensions/replica-diff :seal-task
   [{:keys [args]} old new]
