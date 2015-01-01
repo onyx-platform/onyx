cc8e9ccbaafccd4ee4ac3868389c81ed2dbde714
Michael Drogalis <mjd3089@rit.edu>
2014-12-31 17:45:34 -0800

    Multimethod for reaction in submit job.

--- src/onyx/log/commands/submit_job.clj
+++ src/onyx/log/commands/submit_job.clj
@@ -19,21 +19,30 @@
   [{:keys [args]} old new]
   {:job (:id args)})
 
+(defmulti reallocate?
+  (fn [scheduler old new state]
+    scheduler))
+
+(defmethod reallocate? :onyx.job-scheduler/greedy
+  [scheduler old new state]
+  (not (seq (common/alive-jobs old (:jobs old)))))
+
+(defmethod reallocate? :onyx.job-scheduler/round-robin
+  [scheduler old new state]
+  (if-let [allocation (common/peer->allocated-job (:allocations new) (:id state))]
+    (let [peer-counts (common/balance-jobs new)
+          peers (get (common/job->peers new) (:job allocation))]
+      (when (> (count peers) (get peer-counts (:job allocation)))
+        (let [n (- (count peers) (get peer-counts (:job allocation)))
+              peers-to-drop (common/drop-peers new (:job allocation) n)]
+          (when (some #{(:id state)} (into #{} peers-to-drop))
+            true))))
+    true))
+
 (defmethod extensions/reactions :submit-job
   [entry old new diff peer-args]
-  (cond (and (= (:job-scheduler old) :onyx.job-scheduler/greedy)
-             (not (seq (common/alive-jobs old (:jobs old)))))
-        [{:fn :volunteer-for-task :args {:id (:id peer-args)}}]
-        (= (:job-scheduler old) :onyx.job-scheduler/round-robin)
-        (if-let [allocation (common/peer->allocated-job (:allocations new) (:id peer-args))]
-          (let [peer-counts (common/balance-jobs new)
-                peers (get (common/job->peers new) (:job allocation))]
-            (when (> (count peers) (get peer-counts (:job allocation)))
-              (let [n (- (count peers) (get peer-counts (:job allocation)))
-                    peers-to-drop (common/drop-peers new (:job allocation) n)]
-                (when (some #{(:id peer-args)} (into #{} peers-to-drop))
-                  [{:fn :volunteer-for-task :args {:id (:id peer-args)}}]))))
-          [{:fn :volunteer-for-task :args {:id (:id peer-args)}}])))
+  (when (reallocate? (:job-scheduler old) old new peer-args)
+    [{:fn :volunteer-for-task :args {:id (:id peer-args)}}]))
 
 (defmethod extensions/fire-side-effects! :submit-job
   [entry old new diff state]
