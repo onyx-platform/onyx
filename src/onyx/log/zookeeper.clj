fad1c818fd272f652f6a5700353134da91806c03
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-01-25 02:55:34 +0800

    Fix whitespace.

--- src/onyx/log/zookeeper.clj
+++ src/onyx/log/zookeeper.clj
@@ -93,7 +93,12 @@
         bytes (serialize data)]
     (zk/create conn node :data bytes :persistent? true :sequential? true)))
 
-(defmethod extensions/read-log-entry ZooKeeper                                                                                           [{:keys [conn opts prefix] :as log} position]                                                                                          (let [node (str (log-path prefix) "/entry-" (pad-sequential-id position))                                                                    data (zk/data conn node)                                                                                                               content (deserialize (:data data))]                                                                                                (assoc content :message-id position :created-at (:ctime (:stat data)))))
+(defmethod extensions/read-log-entry ZooKeeper
+  [{:keys [conn opts prefix] :as log} position]
+  (let [node (str (log-path prefix) "/entry-" (pad-sequential-id position))
+        data (zk/data conn node)
+        content (deserialize (:data data))]
+    (assoc content :message-id position :created-at (:ctime (:stat data)))))
 
 (defmethod extensions/register-pulse ZooKeeper
   [{:keys [conn opts prefix] :as log} id]
