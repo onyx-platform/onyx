6852423dd602a4ac90a3be5956355269c119efbe
Michael Drogalis <mjd3089@rit.edu>
2015-01-24 20:30:05 -0800

    Include in test.

--- test/onyx/log/submit_job_test.clj
+++ test/onyx/log/submit_job_test.clj
@@ -28,3 +28,39 @@
   (fact (:jobs new-replica) => [:b :a])
   (fact diff => {:job :a})
   (fact reactions => nil))
+
+(def entry (create-log-entry :submit-job {:id :j1
+                                          :tasks [:t1 :t2 :t3]}))
+
+(def f (partial extensions/apply-log-entry (assoc entry :message-id 0)))
+
+(def rep-diff (partial extensions/replica-diff entry))
+
+(def rep-reactions (partial extensions/reactions entry))
+
+(def old-replica {:job-scheduler :onyx.job-scheduler/round-robin
+                  :peers [:p1 :p2]})
+
+(let [new-replica (f old-replica)
+      diff (rep-diff old-replica new-replica)]
+  (fact (rep-reactions old-replica new-replica diff {:id :p1}) => nil?)
+  (fact (rep-reactions old-replica new-replica diff {:id :p2}) => nil?))
+
+(def entry (create-log-entry :submit-job {:id :j1
+                                          :tasks [:t1 :t2 :t3]}))
+
+(def f (partial extensions/apply-log-entry (assoc entry :message-id 0)))
+
+(def rep-diff (partial extensions/replica-diff entry))
+
+(def rep-reactions (partial extensions/reactions entry))
+
+(def old-replica {:job-scheduler :onyx.job-scheduler/round-robin
+                  :peers [:p1 :p2 :p3]})
+
+(let [new-replica (f old-replica)
+      diff (rep-diff old-replica new-replica)]
+  (fact (rep-reactions old-replica new-replica diff {:id :p1}) =not=> nil?)
+  (fact (rep-reactions old-replica new-replica diff {:id :p2}) =not=> nil?)
+  (fact (rep-reactions old-replica new-replica diff {:id :p3}) =not=> nil?))
+
