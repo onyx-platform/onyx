3b45fccc680590a5f7058417cc19890fb905c9c9
Michael Drogalis <mjd3089@rit.edu>
2015-02-04 16:51:30 -0800

    Moves core.async plugin into core project.

---
+++ src/onyx/plugin/core_async.clj
@@ -0,0 +1,77 @@
+(ns onyx.plugin.core-async
+  (:require [clojure.core.async :refer [>!! <!! alts!! timeout go <!]]
+            [onyx.peer.task-lifecycle-extensions :as l-ext]
+            [onyx.peer.pipeline-extensions :as p-ext]
+            [taoensso.timbre :refer [debug] :as timbre]))
+
+(defmethod l-ext/inject-lifecycle-resources :core.async/read-from-chan
+  [_ event]
+  {:core.async/pending-messages (atom {})})
+
+(defmethod p-ext/read-batch [:input :core.async]
+  [{:keys [onyx.core/task-map core.async/in-chan core.async/pending-messages] :as event}]
+  (let [batch-size (:onyx/batch-size task-map)
+        ms (or (:onyx/batch-timeout task-map) 1000)
+        batch (->> (range batch-size)
+                   (map (fn [_] {:id (java.util.UUID/randomUUID)
+                                :input :core.async
+                                :message (first (alts!! [in-chan (timeout ms)]))}))
+                   (filter (comp not nil? :message)))]
+    (doseq [m batch]
+      (go (try (<! (timeout 5000))
+               (when (get @pending-messages (:id m))
+                 (p-ext/replay-message event (:id m)))
+               (catch Exception e
+                 (taoensso.timbre/warn e))))
+      (swap! pending-messages assoc (:id m) m))
+    {:onyx.core/batch batch}))
+
+(defmethod p-ext/decompress-batch [:input :core.async]
+  [{:keys [onyx.core/batch]}]
+  {:onyx.core/decompressed batch})
+
+(defmethod p-ext/apply-fn [:input :core.async]
+  [event segment]
+  segment)
+
+(defmethod p-ext/ack-message [:input :core.async]
+  [{:keys [core.async/pending-messages]} message-id]
+;;  (swap! pending-messages dissoc message-id)
+  )
+
+(defmethod p-ext/replay-message [:input :core.async]
+  [{:keys [core.async/pending-messages core.async/in-chan]} message-id]
+  (timbre/info (str "Replaying " message-id))
+  (>!! in-chan (:message (get @pending-messages message-id)))
+  (swap! pending-messages dissoc message-id))
+
+(defmethod p-ext/apply-fn [:output :core.async]
+  [event segment]
+  segment)
+
+(defmethod p-ext/compress-batch [:output :core.async]
+  [{:keys [onyx.core/results]}]
+  {:onyx.core/compressed results})
+
+(defmethod p-ext/write-batch [:output :core.async]
+  [{:keys [onyx.core/compressed core.async/out-chan]}]
+  (doseq [segment compressed]
+    (>!! out-chan (:message segment)))
+  {})
+
+(defmethod p-ext/seal-resource [:output :core.async]
+  [{:keys [core.async/out-chan]}]
+  (>!! out-chan :done)
+  {})
+
+(defn take-segments!
+  "Takes segments off the channel until :done is found.
+   Returns a seq of segments, including :done."
+  [ch]
+  (loop [x []]
+    (let [segment (<!! ch)]
+      (let [stack (conj x segment)]
+        (if-not (= segment :done)
+          (recur stack)
+          stack)))))
+
--- test/onyx/messaging/base_test.clj
+++ test/onyx/messaging/base_test.clj
@@ -1,8 +1,8 @@
 (ns onyx.messaging.base-test
-  (:require [midje.sweet :refer :all]
-            [onyx.system :refer [onyx-development-env]]
-            [onyx.queue.hornetq-utils :as hq-util]
-            [onyx.plugin.hornetq]
+  (:require [clojure.core.async :refer [chan >!! <!!]]
+            [midje.sweet :refer :all]
+            [onyx.peer.task-lifecycle-extensions :as l-ext]
+            [onyx.plugin.core-async]
             [onyx.api]))
 
 (def id (java.util.UUID/randomUUID))
@@ -22,13 +22,7 @@
    :onyx.messaging/impl messaging})
 
 (def peer-config
-  {:hornetq/mode :udp
-   :hornetq.udp/cluster-name (:cluster-name (:hornetq config))
-   :hornetq.udp/group-address (:group-address (:hornetq config))
-   :hornetq.udp/group-port (:group-port (:hornetq config))
-   :hornetq.udp/refresh-timeout (:refresh-timeout (:hornetq config))
-   :hornetq.udp/discovery-timeout (:discovery-timeout (:hornetq config))
-   :zookeeper/address (:address (:zookeeper config))
+  {:zookeeper/address (:address (:zookeeper config))
    :onyx/id id
    :onyx.peer/inbox-capacity (:inbox-capacity (:peer config))
    :onyx.peer/outbox-capacity (:outbox-capacity (:peer config))
@@ -42,39 +36,19 @@
 
 (def batch-size 10)
 
-(def echo 10)
-
-(def hornetq-host "localhost")
-
-(def hornetq-port 5465)
-
-(def hq-config {"host" hornetq-host "port" hornetq-port})
-
-(def in-queue (str (java.util.UUID/randomUUID)))
-
-(def out-queue (str (java.util.UUID/randomUUID)))
-
-(hq-util/create-queue! hq-config in-queue)
-(hq-util/create-queue! hq-config out-queue)
-
-(hq-util/write! hq-config in-queue (map (fn [x] {:n x}) (range n-messages)) echo)
-
 (defn my-inc [segment]
-  (if (= (:n segment) 50)
-    (do (prn "Chucking " segment)
-        [])
-    (assoc segment :n (inc (:n segment)))))
+  (prn "-->" segment)
+  (assoc segment :n (inc (:n segment))))
 
 (def catalog
   [{:onyx/name :in
-    :onyx/ident :hornetq/read-segments
+    :onyx/ident :core.async/read-from-chan
     :onyx/type :input
-    :onyx/medium :hornetq
+    :onyx/medium :core.async
     :onyx/consumption :concurrent
-    :hornetq/queue-name in-queue
-    :hornetq/host (:host (:non-clustered (:hornetq config)))
-    :hornetq/port (:port (:non-clustered (:hornetq config)))
-    :onyx/batch-size batch-size}
+    :onyx/batch-size batch-size
+    :onyx/max-peers 1
+    :onyx/doc "Reads segments from a core.async channel"}
 
    {:onyx/name :inc
     :onyx/fn :onyx.messaging.base-test/my-inc
@@ -83,16 +57,25 @@
     :onyx/batch-size batch-size}
 
    {:onyx/name :out
-    :onyx/ident :hornetq/write-segments
+    :onyx/ident :core.async/write-to-chan
     :onyx/type :output
-    :onyx/medium :hornetq
+    :onyx/medium :core.async
     :onyx/consumption :concurrent
-    :hornetq/queue-name out-queue
-    :hornetq/host (:host (:non-clustered (:hornetq config)))
-    :hornetq/port (:port (:non-clustered (:hornetq config)))
-    :onyx/batch-size batch-size}])
+    :onyx/batch-size batch-size
+    :onyx/max-peers 1
+     :onyx/doc "Writes segments to a core.async channel"}])
 
 (def workflow [[:in :inc] [:inc :out]])
+
+(def in-chan (chan 100))
+
+(def out-chan (chan 100))
+
+(defmethod l-ext/inject-lifecycle-resources :in
+  [_ _] {:core.async/in-chan in-chan})
+
+(defmethod l-ext/inject-lifecycle-resources :out
+  [_ _] {:core.async/out-chan out-chan})
 
 (def v-peers (onyx.api/start-peers! 3 peer-config))
 
@@ -101,7 +84,8 @@
  {:catalog catalog :workflow workflow
   :task-scheduler :onyx.task-scheduler/round-robin})
 
-(def results (hq-util/read! hq-config out-queue n-messages echo))
+(doseq [n (range 25)]
+  (>!! in-chan {:n n}))
 
 (comment
   (doseq [v-peer v-peers]
