bc28a22f15bad5e0de1622a04905f9959ea9f365
Michael Drogalis <mjd3089@rit.edu>
2015-02-14 09:50:05 -0800

    0.5.3-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.mdrogalis/onyx "0.5.2"
+(defproject com.mdrogalis/onyx "0.5.3-SNAPSHOT"
   :description "Distributed, masterless, fault tolerant data processing for Clojure"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
