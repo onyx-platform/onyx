0f30e8a1bc3bf49f53af16520634858baf409d4f
sbennett33 <scott@readyreplay.com>
2015-03-11 22:19:41 -0700

    Only log errors and above to stdout
    This configuration should only append errors to stdout by default

--- src/onyx/logging_configuration.clj
+++ src/onyx/logging_configuration.clj
@@ -20,7 +20,9 @@
         (timbre/set-config!
           [:shared-appender-config :rotor]
           {:path file :max-size (* 512 10240) :backlog 5})
-        (timbre/set-config! [:appenders :standard-out :enabled?] true)
+        (timbre/set-config! [:appenders :standard-out]
+                            {:min-level :error
+                             :enabled? true})
         (timbre/set-config! [:appenders :spit :enabled?] false)))
 
     (info "Starting Logging Configuration")
