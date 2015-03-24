ee92a10a1a9b2bbcbd7d83f12c5d3bb20dbed4eb
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-03-24 15:36:23 +0800

    Add messaging to notify-join test. Switch defs to let

--- test/onyx/log/notify_join_cluster_test.clj
+++ test/onyx/log/notify_join_cluster_test.clj
@@ -1,24 +1,32 @@
 (ns onyx.log.notify-join-cluster-test
   (:require [onyx.extensions :as extensions]
             [onyx.log.entry :refer [create-log-entry]]
+            [onyx.messaging.aeron :as aeron]
+            [com.stuartsierra.component :as component]
             [midje.sweet :refer :all]))
 
-(def entry (create-log-entry :notify-join-cluster {:observer :d :subject :a}))
 
-(def f (partial extensions/apply-log-entry entry))
+(def peer-config 
+  (:peer-config (read-string (slurp (clojure.java.io/resource "test-config.edn")))))
 
-(def rep-diff (partial extensions/replica-diff entry))
+(def messaging 
+  (aeron/aeron {:opts peer-config}))
 
-(def rep-reactions (partial extensions/reactions entry))
+(component/start messaging)
+(try
+  (let [entry (create-log-entry :notify-join-cluster {:observer :d :subject :a})
+        f (partial extensions/apply-log-entry entry)
+        rep-diff (partial extensions/replica-diff entry)
+        rep-reactions (partial extensions/reactions entry)
+        old-replica {:pairs {:a :b :b :c :c :a} :prepared {:a :d} :peers [:a :b :c]}
+        new-replica (f old-replica)
+        diff (rep-diff old-replica new-replica)
+        reactions (rep-reactions old-replica new-replica diff {:id :d :messenger messaging})]
+    (fact diff => {:observer :d :subject :b :accepted-joiner :d :accepted-observer :a})
+    (fact reactions => [{:fn :accept-join-cluster :args diff :immediate? true :site-resources {:aeron/port 40201}}])
+    (fact (rep-reactions old-replica new-replica diff {:id :a}) => nil)
+    (fact (rep-reactions old-replica new-replica diff {:id :b}) => nil)
+    (fact (rep-reactions old-replica new-replica diff {:id :c}) => nil))
 
-(def old-replica {:pairs {:a :b :b :c :c :a} :prepared {:a :d} :peers [:a :b :c]})
-
-(let [new-replica (f old-replica)
-      diff (rep-diff old-replica new-replica)
-      reactions (rep-reactions old-replica new-replica diff {:id :d})]
-  (fact diff => {:observer :d :subject :b :accepted-joiner :d :accepted-observer :a})
-  (fact reactions => [{:fn :accept-join-cluster :args diff :immediate? true}])
-  (fact (rep-reactions old-replica new-replica diff {:id :a}) => nil)
-  (fact (rep-reactions old-replica new-replica diff {:id :b}) => nil)
-  (fact (rep-reactions old-replica new-replica diff {:id :c}) => nil))
-
+  (finally
+    (component/stop messaging)))
