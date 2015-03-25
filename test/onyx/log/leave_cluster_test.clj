c26cec66f9e73540bc9812d6e421ebf0643de450
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-03-25 16:31:27 +0800

    Use less defs, missing fact call

--- test/onyx/log/leave_cluster_test.clj
+++ test/onyx/log/leave_cluster_test.clj
@@ -4,17 +4,12 @@
             [onyx.system]
             [midje.sweet :refer :all]))
 
-(def entry (create-log-entry :leave-cluster {:id :c}))
-
-(def f (partial extensions/apply-log-entry entry))
-
-(def rep-diff (partial extensions/replica-diff entry))
-
-(def rep-reactions (partial extensions/reactions entry))
-
-(def old-replica {:pairs {:a :b :b :c :c :a} :peers [:a :b :c]})
-
-(let [new-replica (f old-replica)
+(let [entry (create-log-entry :leave-cluster {:id :c})
+      f (partial extensions/apply-log-entry entry)
+      rep-diff (partial extensions/replica-diff entry)
+      rep-reactions (partial extensions/reactions entry)
+      old-replica {:pairs {:a :b :b :c :c :a} :peers [:a :b :c]}
+      new-replica (f old-replica)
       diff (rep-diff old-replica new-replica)]
   (fact (get-in new-replica [:pairs :a]) => :b)
   (fact (get-in new-replica [:pairs :b]) => :a)
@@ -22,82 +17,65 @@
   (fact diff => {:died :c :updated-watch {:observer :b :subject :a}})
   (fact (rep-reactions old-replica new-replica diff {:id :a}) => nil))
 
-(def entry (create-log-entry :leave-cluster {:id :b}))
-
-(def f (partial extensions/apply-log-entry entry))
-
-(def rep-diff (partial extensions/replica-diff entry))
-
-(def rep-reactions (partial extensions/reactions entry))
-
-(def old-replica {:pairs {:a :b :b :a} :peers [:a :b]})
-
-(let [new-replica (f old-replica)
+(let [entry (create-log-entry :leave-cluster {:id :b})
+      f (partial extensions/apply-log-entry entry)
+      rep-diff (partial extensions/replica-diff entry)
+      rep-reactions (partial extensions/reactions entry)
+      old-replica {:pairs {:a :b :b :a} :peers [:a :b]}
+      new-replica (f old-replica)
       diff (rep-diff old-replica new-replica)]
   (fact (get-in new-replica [:pairs :a]) => nil)
   (fact (get-in new-replica [:pairs :b]) => nil)
   (fact diff => {:died :b :updated-watch {:observer :a :subject :a}})
   (fact (rep-reactions old-replica new-replica diff {:id :a}) => nil))
 
-(def entry (create-log-entry :leave-cluster {:id :d}))
-
-(def f (partial extensions/apply-log-entry entry))
-
-(def rep-diff (partial extensions/replica-diff entry))
-
-(def rep-reactions (partial extensions/reactions entry))
-
-(def old-replica {:job-scheduler :onyx.job-scheduler/round-robin
-                  :pairs {:a :b :b :c :c :d :d :a} 
-                  :peers [:a :b :c :d]
-                  :jobs [:j1 :j2]
-                  :task-schedulers {:j1 :onyx.task-scheduler/round-robin
-                                    :j2 :onyx.task-scheduler/round-robin} 
-                  :tasks {:j1 [:t1] :j2 [:t2]}
-                  :allocations {:j1 {:t1 [:a :b]}
-                                :j2 {:t2 [:c :d]}}})
-
-(let [new-replica (f old-replica)
+(let [entry (create-log-entry :leave-cluster {:id :d})
+      f (partial extensions/apply-log-entry entry)
+      rep-diff (partial extensions/replica-diff entry)
+      rep-reactions (partial extensions/reactions entry)
+      old-replica {:job-scheduler :onyx.job-scheduler/round-robin
+                   :pairs {:a :b :b :c :c :d :d :a} 
+                   :peers [:a :b :c :d]
+                   :jobs [:j1 :j2]
+                   :task-schedulers {:j1 :onyx.task-scheduler/round-robin
+                                     :j2 :onyx.task-scheduler/round-robin} 
+                   :tasks {:j1 [:t1] :j2 [:t2]}
+                   :allocations {:j1 {:t1 [:a :b]}
+                                 :j2 {:t2 [:c :d]}}}
+      new-replica (f old-replica)
       diff (rep-diff old-replica new-replica)]
-  (:allocations (f old-replica)) => {:j1 {:t1 [:a :b]} :j2 {:t2 [:c]}}
+  (fact (:allocations (f old-replica)) => {:j1 {:t1 [:a :b]} :j2 {:t2 [:c]}})
   (fact (rep-reactions old-replica new-replica diff {:id :a}) => nil)
   (fact (rep-reactions old-replica new-replica diff {:id :b}) => nil)
   (fact (rep-reactions old-replica new-replica diff {:id :c}) => nil))
 
-(def entry (create-log-entry :leave-cluster {:id :c}))
-
-(def f (partial extensions/apply-log-entry entry))
-
-(def rep-diff (partial extensions/replica-diff entry))
-
-(def rep-reactions (partial extensions/reactions entry))
-
-(def old-replica {:job-scheduler :onyx.job-scheduler/round-robin
-                  :pairs {:a :b :b :c :c :a} 
-                  :peers [:a :b :c]
-                  :jobs [:j1 :j2]
-                  :task-schedulers {:j1 :onyx.task-scheduler/round-robin
-                                    :j2 :onyx.task-scheduler/round-robin} 
-                  :tasks {:j1 [:t1] :j2 [:t2]}
-                  :allocations {:j1 {:t1 [:a :b]} :j2 {:t2 [:c]}}})
-
-(let [new-replica (f old-replica)
+(let [entry (create-log-entry :leave-cluster {:id :c})
+      f (partial extensions/apply-log-entry entry)
+      rep-diff (partial extensions/replica-diff entry)
+      rep-reactions (partial extensions/reactions entry)
+      old-replica {:job-scheduler :onyx.job-scheduler/round-robin
+                   :pairs {:a :b :b :c :c :a} 
+                   :peers [:a :b :c]
+                   :jobs [:j1 :j2]
+                   :task-schedulers {:j1 :onyx.task-scheduler/round-robin
+                                     :j2 :onyx.task-scheduler/round-robin} 
+                   :tasks {:j1 [:t1] :j2 [:t2]}
+                   :allocations {:j1 {:t1 [:a :b]} :j2 {:t2 [:c]}}}
+      new-replica (f old-replica)
       diff (rep-diff old-replica new-replica)]
-  (:allocations (f old-replica)) => {:j1 {:t1 [:a :b]} :j2 {:t2 []}}
+  (fact (:allocations (f old-replica)) => {:j1 {:t1 [:a :b]} :j2 {:t2 []}})
   (fact (rep-reactions old-replica new-replica diff {:id :a}) => nil)
   (fact (rep-reactions old-replica new-replica diff {:id :b})
-        => [{:fn :volunteer-for-task :args {:id :b}}]))
+        => [{:fn :volunteer-for-task :args {:id :b}}])
 
-(def old-replica {:job-scheduler :onyx.job-scheduler/round-robin
-                  :pairs {:a :b :b :c :c :a} 
-                  :peers [:a :b :c]
-                  :jobs [:j1 :j2]
-                  :task-schedulers {:j1 :onyx.task-scheduler/round-robin
-                                    :j2 :onyx.task-scheduler/round-robin} 
-                  :tasks {:j1 [:t1] :j2 [:t2]}
-                  :allocations {:j1 {:t1 [:a :b]} :j2 {:t2 [:c]}}
-                  :sealing-tasks {:t2 :c}})
-
-(let [new-replica (f old-replica)]
-  (:sealing-tasks new-replica) => {})
-
+  (let [old-replica2 {:job-scheduler :onyx.job-scheduler/round-robin
+                      :pairs {:a :b :b :c :c :a} 
+                      :peers [:a :b :c]
+                      :jobs [:j1 :j2]
+                      :task-schedulers {:j1 :onyx.task-scheduler/round-robin
+                                        :j2 :onyx.task-scheduler/round-robin} 
+                      :tasks {:j1 [:t1] :j2 [:t2]}
+                      :allocations {:j1 {:t1 [:a :b]} :j2 {:t2 [:c]}}
+                      :sealing-tasks {:t2 :c}}
+        new-replica2 (f old-replica2)]
+    (fact (:sealing-tasks new-replica2) => {})))
