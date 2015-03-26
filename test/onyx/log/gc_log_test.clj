930ce2d6a46fb930df06e919beb2e70472631a62
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-03-26 19:31:34 +0800

    Add peer group to gc test, shutdown all vpeers.

--- test/onyx/log/gc_log_test.clj
+++ test/onyx/log/gc_log_test.clj
@@ -17,13 +17,15 @@
 
 (def env (onyx.api/start-env env-config))
 
+(def peer-group (onyx.api/start-peer-group peer-config))
+
 (def n-peers 5)
 
-(def v-peers (onyx.api/start-peers n-peers peer-config))
+(def v-peers (onyx.api/start-peers n-peers peer-group))
 
 (onyx.api/gc peer-config)
 
-(def v-peers (onyx.api/start-peers n-peers peer-config))
+(def v-peers2 (onyx.api/start-peers n-peers peer-group))
 
 (def ch (chan 100))
 
@@ -36,8 +38,8 @@
 
 (fact "Starting peers after GC succeeded" true => true)
 
-(doseq [v-peer v-peers]
+(doseq [v-peer (concat v-peers v-peers2)]
   (onyx.api/shutdown-peer v-peer))
 
 (onyx.api/shutdown-env env)
-
+(onyx.api/shutdown-peer-group peer-group)
