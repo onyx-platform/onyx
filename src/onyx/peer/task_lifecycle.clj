f66f6b650bf237b62a0b26071ad34b20200d5ee3
Michael Drogalis <mjd3089@rit.edu>
2015-04-24 20:11:54 -0700

    Fall through to identity.

--- src/onyx/peer/task_lifecycle.clj
+++ src/onyx/peer/task_lifecycle.clj
@@ -385,7 +385,7 @@
   (let [matched (filter #(= (:lifecycle/task %) task-name) lifecycles)]
     (reduce
      (fn [f lifecycle]
-       (comp (fn [x] ((operation/kw->fn (get lifecycle kw)) x lifecycle)) f))
+       (comp (fn [x] ((operation/kw->fn (get lifecycle kw :clojure.core/identity)) x lifecycle)) f))
      identity
      matched)))
 
