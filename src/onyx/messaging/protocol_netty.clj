8cadf7cb251d7cb276b8f94a78ce0bbf25d23821
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-06 22:51:25 +0800

    Perf: improve netty segment reading / writing.

--- src/onyx/messaging/protocol_netty.clj
+++ src/onyx/messaging/protocol_netty.clj
@@ -1,5 +1,6 @@
 (ns ^:no-doc onyx.messaging.protocol-netty
-    (:require [taoensso.timbre :as timbre])
+    (:require [taoensso.timbre :as timbre]
+              [onyx.types :refer [->Leaf]])
     (:import [java.util UUID]
              [io.netty.buffer ByteBuf Unpooled UnpooledByteBufAllocator
               PooledByteBufAllocator ByteBufAllocator]))
@@ -38,8 +39,8 @@
 (def ^:const ack-base-length (int 40))
 
 ; message length without nippy segments
-; id (uuid), acker-id (uuid), completion-id (uuid), ack-val (long), nippy-byte-count (int)
-(def ^:const message-base-length (int 60))
+; id (uuid), acker-id (uuid), completion-id (uuid), ack-val (long)
+(def ^:const message-base-length (int 56))
 
 ; messages with 0 messages in it
 (def ^:const messages-base-length (int 4))
@@ -97,48 +98,42 @@
   (write-uuid buf id)
   (write-uuid buf acker-id)
   (write-uuid buf completion-id)
-  (.writeLong buf ack-val)
-  ; this could probably be a short
-  (.writeInt buf (alength ^bytes message)) 
-  (.writeBytes buf ^bytes message))
+  (.writeLong buf ack-val))
 
-(defn read-message-buf [decompress-f ^ByteBuf buf]
+(defn read-message-buf [decompress-f ^ByteBuf buf message]
   (let [id (take-uuid buf)
         acker-id (take-uuid buf)
         completion-id (take-uuid buf)
-        ack-val (.readLong buf)
-        message-size (.readInt buf)
-        arr (byte-array message-size)
-        _ (.readBytes buf arr)
-        message (decompress-f arr)]
-    {:id id 
-     :acker-id acker-id 
-     :completion-id completion-id 
-     :ack-val ack-val 
-     :message message}))
+        ack-val (.readLong buf)]
+    ;;; Maybe should have a new record type for the read messages
+    (->Leaf message id acker-id completion-id ack-val nil nil nil nil)))
 
 (defn build-messages-msg-buf [compress-f messages] 
-  (let [compressed-messages (map (fn [msg]
-                                   (update-in msg [:message] compress-f))
-                                 messages)
+  (let [message-bytes ^bytes (compress-f (map :message messages))
+        message-count (int (count messages))
         buf-size (+ type-header-length
                     (+ 4 ; message count int
-                       (* (count messages) message-base-length)
-                       ; nippy compressed segments
-                       (apply + (map (fn [m]
-                                       (alength ^bytes (:message m)))  
-                                     compressed-messages))))
+                       (alength message-bytes)
+                       (* message-base-length message-count)))
         buf ^ByteBuf (byte-buffer buf-size)] 
     (.writeByte buf messages-type-id) ; message type header
-    (.writeInt buf (int (count compressed-messages))) ; number of messages
-    (doseq [msg compressed-messages]
+    (.writeInt buf message-count) ; number of messages
+    (.writeInt buf (alength message-bytes)) ; nippy compressed data size
+    (.writeBytes buf message-bytes) ; nippy compressed messages
+    (doseq [msg messages]
       (write-message-msg buf msg))
     buf))
 
 (defn read-messages-buf [decompress-f ^ByteBuf buf]
   {:type messages-type-id 
-   :messages (let [message-count (.readInt buf)]
-               (doall (repeatedly message-count #(read-message-buf decompress-f buf))))})
+   :messages (let [message-count (.readInt buf)
+                   messages-payload-size (.readInt buf)
+                   arr (byte-array messages-payload-size)
+                   _ (.readBytes buf arr)
+                   messages (decompress-f arr)]
+               (doall (map (fn [msg] 
+                             (read-message-buf decompress-f buf msg)) 
+                           messages)))})
 
 (defn read-buf [decompress-f ^ByteBuf buf]
   (let [msg-type ^byte (.readByte buf)] 
