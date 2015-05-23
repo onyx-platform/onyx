a6125dc8b9a1571277db4e78873fe306007a82fa
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-23 16:42:32 +0800

    Run generative tests on all nodes to smooth out run times over nodes. Make branch detection support codeship. Decrease branch factor now that gen tests are running on all nodes.

--- circle-runner.sh
+++ circle-runner.sh
@@ -2,19 +2,24 @@
 
 set -e
 
-if [ $CIRCLE_BRANCH == "master" ]; then
-	export TEST_CHECK_FACTOR=20
+
+if [ -z "$CIRCLE_BRANCH" ]; then
+	export BR=$CI_BRANCH
+else
+	export BR=$CIRCLE_BRANCH
 fi
 
+if [ $BR == "master" ]; then
+	export TEST_CHECK_FACTOR=5
+fi
 
 i=0
 files=""
 
-TEST_NSES=$(find test -name "*.clj" |sed s/test\\///|sed s/\\//\\./g|sed s/".clj$"//|sed s/"_"/"-"/g|tr '\n' ' '|sort)
+TEST_NSES_NONGEN=$(find test -name "*.clj" |sed s/test\\///|sed s/\\//\\./g|sed s/".clj$"//|sed s/"_"/"-"/g|grep -v gen | tr '\n' ' '|sort)
+TEST_NSES_GENERATIVE=$(find test -name "*.clj" |sed s/test\\///|sed s/\\//\\./g|sed s/".clj$"//|sed s/"_"/"-"/g | grep gen | tr '\n' ' '|sort)
 
-echo "Testing " $TEST_NSES
-
-for file in $TEST_NSES
+for file in $TEST_NSES_NONGEN
 do
   if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
   then
@@ -22,6 +27,12 @@ do
   fi
   ((++i))
 done
+
+# Run generative tests on all nodes to more evently distribute run time
+# If they're taking too long we can reduce the TEST_CHECK_FACTOR
+files+=" "$TEST_NSES_GENERATIVE
+
+echo "Running " $files
 
 export TEST_TRANSPORT_IMPL=$1 
 lein with-profile dev,circle-ci midje $files
