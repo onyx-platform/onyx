f0859f4db43f9bad602dc057c5580afdd16bb585
Michael Drogalis <mjd3089@rit.edu>
2015-05-23 19:31:52 -0700

    Mimic another test.

--- test/onyx/log/percentage_grouping_generative_test.clj
+++ test/onyx/log/percentage_grouping_generative_test.clj
@@ -54,6 +54,36 @@
               :onyx/doc "Writes segments to a core.async channel"}]
    :task-scheduler :onyx.task-scheduler/percentage})
 
+(def job-2-id #uuid "60180f08-60b9-4584-9900-93dbbe2c3905")
+
+(def job-2
+  {:workflow [[:a :b] [:b :c]]
+   :catalog [{:onyx/name :a
+              :onyx/ident :core.async/read-from-chan
+              :onyx/type :input
+              :onyx/medium :core.async
+              :onyx/percentage 20
+              :onyx/batch-size 20
+              :onyx/doc "Reads segments from a core.async channel"}
+
+             {:onyx/name :b
+              :onyx/fn :mock/fn
+              :onyx/type :function
+              :onyx/group-by-kw :mock-key
+              :onyx/min-peers 4
+              :onyx/flux-policy :kill
+              :onyx/percentage 60
+              :onyx/batch-size 20}
+
+             {:onyx/name :c
+              :onyx/ident :core.async/write-to-chan
+              :onyx/type :output
+              :onyx/medium :core.async
+              :onyx/percentage 20
+              :onyx/batch-size 20
+              :onyx/doc "Writes segments to a core.async channel"}]
+   :task-scheduler :onyx.task-scheduler/percentage})
+
 (deftest min-peers-one-job-upper-bound
   (let [rets (api/create-submit-job-entry
               job-1-id
@@ -74,9 +104,31 @@
          :log []
          :peer-choices []}))]
      (let [[t1 t2 t3] (:tasks (:args rets))]
-       ;; (prn "--")
-       ;; (clojure.pprint/pprint replica)
-       ;; (prn "--")
        (is (= 2 (count (get (get (:allocations replica) job-1-id) t1))))
        (is (= 4 (count (get (get (:allocations replica) job-1-id) t2))))
        (is (= 4 (count (get (get (:allocations replica) job-1-id) t3))))))))
+
+(deftest min-peers-one-job-no-upper-bound
+  (let [rets (api/create-submit-job-entry
+              job-2-id
+              peer-config 
+              job-2
+              (planning/discover-tasks (:catalog job-2) (:workflow job-2)))]
+    (checking
+     "Checking that 6 tasks are assigned to B."
+     (times 50)
+     [{:keys [replica log peer-choices]}
+      (log-gen/apply-entries-gen
+       (gen/return
+        {:replica {:job-scheduler :onyx.job-scheduler/greedy
+                   :messaging {:onyx.messaging/impl :dummy-messenger}}
+         :message-id 0
+         :entries (assoc (log-gen/generate-join-queues (log-gen/generate-peer-ids 10)) :job-2 [rets])
+         :log []
+         :peer-choices []}))]
+     (let [[t1 t2 t3] (:tasks (:args rets))
+           c1 (count (get (get (:allocations replica) job-2-id) t1))
+           c2 (count (get (get (:allocations replica) job-2-id) t2))
+           c3 (count (get (get (:allocations replica) job-2-id) t3))]
+       (is (>= c2 4))
+       (is (= 10 (+ c1 c2 c3)))))))
