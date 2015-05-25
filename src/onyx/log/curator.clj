9a414629faccc46e8595d9b274b3310f49b818bf
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-25 14:03:52 +0800

    Eliminate reflection warnings. Bring in some zookeeper-clj functions

--- src/onyx/log/curator.clj
+++ src/onyx/log/curator.clj
@@ -1,19 +1,50 @@
 (ns onyx.log.curator
   (:require [clojure.core.async :refer [chan >!! <!! close! thread]]
             [taoensso.timbre :refer [fatal warn trace]]
-            [onyx.static.default-vals :refer [defaults]]
-            [zookeeper :as zk]
-            [zookeeper.internal :as zi])
+            [onyx.static.default-vals :refer [defaults]])
   (:import [org.apache.zookeeper CreateMode]
-           [org.apache.zookeeper KeeperException$NoNodeException KeeperException$NodeExistsException]
+           [org.apache.zookeeper KeeperException$NoNodeException KeeperException$NodeExistsException Watcher]
            [org.apache.curator.test TestingServer]
            [org.apache.zookeeper.data Stat]
            [org.apache.curator.framework CuratorFrameworkFactory CuratorFramework]
-           [org.apache.curator.framework.api CuratorWatcher PathAndBytesable Versionable]
+           [org.apache.curator.framework.api CuratorWatcher PathAndBytesable Versionable GetDataBuilder 
+            SetDataBuilder DeleteBuilder ExistsBuilder GetChildrenBuilder Pathable Watchable]
            [org.apache.curator.framework.state ConnectionStateListener ConnectionState]
            [org.apache.curator.framework.imps CuratorFrameworkState]
            [org.apache.curator RetryPolicy]
            [org.apache.curator.retry BoundedExponentialBackoffRetry]))
+
+;; Thanks to zookeeper-clj
+(defn stat-to-map
+  ([^org.apache.zookeeper.data.Stat stat]
+   ;;(long czxid, long mzxid, long ctime, long mtime, int version, int cversion, int aversion, long ephemeralOwner, int dataLength, int numChildren, long pzxid)
+   (when stat
+     {:czxid (.getCzxid stat)
+      :mzxid (.getMzxid stat)
+      :ctime (.getCtime stat)
+      :mtime (.getMtime stat)
+      :version (.getVersion stat)
+      :cversion (.getCversion stat)
+      :aversion (.getAversion stat)
+      :ephemeralOwner (.getEphemeralOwner stat)
+      :dataLength (.getDataLength stat)
+      :numChildren (.getNumChildren stat)
+      :pzxid (.getPzxid stat)})))
+
+(defn event-to-map
+  ([^org.apache.zookeeper.WatchedEvent event]
+     (when event
+       {:event-type (keyword (.name (.getType event)))
+        :keeper-state (keyword (.name (.getState event)))
+        :path (.getPath event)})))
+
+;; Watcher
+
+(defn make-watcher
+  ([handler]
+     (reify Watcher
+       (process [this event]
+         (handler (event-to-map event))))))
 
 (defn ^CuratorFramework connect
   ([connection-string]
@@ -51,12 +82,12 @@
 (defn create
   [^CuratorFramework client path & {:keys [data] :as opts}]
   (try 
-    (let [cr (.. client 
+    (let [cr ^SetDataBuilder (.. client 
                  create 
                  (withMode (create-mode opts)))]
       (if data 
-        (.forPath cr path data)
-        (.forPath cr path)))
+        (.forPath ^SetDataBuilder cr path data)
+        (.forPath ^SetDataBuilder cr path)))
     (catch org.apache.zookeeper.KeeperException$NodeExistsException e
       false)))
 
@@ -68,8 +99,8 @@
                  creatingParentsIfNeeded 
                  (withMode (create-mode opts)))]
       (if data 
-        (.forPath cr path data)
-        (.forPath cr path)))
+        (.forPath ^SetDataBuilder cr path data)
+        (.forPath ^SetDataBuilder cr path)))
     (catch org.apache.zookeeper.KeeperException$NodeExistsException e
       false) ))
 
@@ -77,30 +108,31 @@
   "Deletes the given node if it exists. Otherwise returns false."
   [^CuratorFramework client path]
   (try
-    (.. client (delete) (forPath path))
+    (.forPath ^DeleteBuilder (.delete client) path )
     (catch KeeperException$NoNodeException e false)))
 
 (defn children 
-  ([^CuratorFramework curator path & {:keys [watcher]}]
-   (.forPath 
-     (cond-> (.getChildren curator)
-       watcher (.usingWatcher (zi/make-watcher watcher))) 
-     path)))
+  ([^CuratorFramework client path & {:keys [watcher]}]
+   (let [children-builder ^GetChildrenBuilder (.getChildren client)]
+     (if watcher 
+       (.forPath ^GetChildrenBuilder (.usingWatcher children-builder ^Watcher (make-watcher watcher)) path)
+       (.forPath ^GetChildrenBuilder children-builder path)))))
 
-(defn data [client path]
-  (let [stat (Stat.)
-        data (.forPath (.storingStatIn (.getData client) stat) path)] 
+(defn data [^CuratorFramework client path]
+  (let [stat ^Stat (Stat.)
+        data (.forPath ^GetDataBuilder (.storingStatIn ^GetDataBuilder (.getData client) stat) path)] 
     {:data data
-     :stat (zi/stat-to-map stat)}))
+     :stat (stat-to-map stat)}))
 
-(defn set-data [client path data version]
-  (.. client 
-      (setData)
-      (withVersion version)
-      (forPath path data)))
+(defn set-data [^CuratorFramework client path data version]
+  (.forPath ^SetDataBuilder (.withVersion ^SetDataBuilder (.setData client) 
+                                          version) 
+            path 
+            data))
 
-(defn exists [client path & {:keys [watcher]}]
-  (zi/stat-to-map 
-    (cond-> (.. client checkExists)
-      watcher (.usingWatcher (zi/make-watcher watcher))
-      true (.forPath path)))) 
+(defn exists [^CuratorFramework client path & {:keys [watcher]}]
+  (stat-to-map 
+    (let [builder ^ExistsBuilder (.. client checkExists)]
+      (if watcher
+        (.forPath ^ExistsBuilder (.usingWatcher builder ^Watcher (make-watcher watcher)) path)
+        (.forPath builder path))))) 
