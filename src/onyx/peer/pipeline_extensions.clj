a01b620c01c4b428efedacbf7166ca336ffe44db
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-06-25 11:22:49 +0800

    Re-add pipeline docstrings.

--- src/onyx/peer/pipeline_extensions.clj
+++ src/onyx/peer/pipeline_extensions.clj
@@ -16,15 +16,16 @@
             [sealResource [clojure.lang.IPersistentMap] Object]])
 
 (defprotocol PipelineInput 
-  ;   "Acknowledges a message at the native level for a batch of message ids.
-  ;    Must return a map."
-  (ack-message [this event message-id])
-  ;   "Releases a message id from the pending message pool and retries it."
-  (retry-message [this event message-id])
-  ;   "Returns true if this message ID is pending."
-  (pending? [this event message-id])
-  ;   "Returns true if this input resource has been exhausted."
-  (drained? [this event]))
+  "Input pipeline protocol. All input pipelines must implement this protocol."
+  (ack-message [this event message-id]
+               "Acknowledges a message at the native level for a batch of message ids.
+               Must return a map.")
+  (retry-message [this event message-id]
+                 "Releases a message id from the pending message pool and retries it.")
+  (pending? [this event message-id]
+            "Returns true if this message ID is pending.")
+  (drained? [this event]
+            "Returns true if this input resource has been exhausted."))
 
 (extend-protocol PipelineInput
   onyx.peer.IPipelineInput
@@ -38,23 +39,24 @@
     (.isDrained this event)))
 
 (defprotocol Pipeline 
-  ;   "Reads :onyx/batch-size segments off the incoming data source.
-  ;    Must return a map with key :onyx.core/batch and value seq representing
-  ;    the ingested segments. The seq must be maps of two keys:
+  "Pipeline protocol. All pipelines must implement this protocols i.e. input, output, functions"
+  (read-batch [this event]
+              "Reads :onyx/batch-size segments off the incoming data source.
+              Must return a map with key :onyx.core/batch and value seq representing
+              the ingested segments. The seq must be maps of two keys:
 
-  ;    - :input - A keyword representing the task that the message came from
-  ;    - :message - The consumed message"
-  (read-batch [this event])
-  ;   "Writes segments to the outgoing data source. Must return a map."
-  (write-batch [this event])
-  ;   "Closes any resources that remain open during a task being executed.
-  ;    Called once at the end of a task for each virtual peer after the incoming
-  ;    queue has been exhausted. Only called once globally for a single task."
+              - :input - A keyword representing the task that the message came from
+              - :message - The consumed message")
+  (write-batch [this event]
+               "Writes segments to the outgoing data source. Must return a map.")
+  (seal-resource [this event]
+                 "Closes any resources that remain open during a task being executed.
+                 Called once at the end of a task for each virtual peer after the incoming
+                 queue has been exhausted. Only called once globally for a single task."
 
-  ;   "Closes any resources that remain open during a task being executed.
-  ;    Called once at the end of a task for each virtual peer after the incoming
-  ;    queue has been exhausted. Only called once globally for a single task."
-  (seal-resource [this event]))
+                 "Closes any resources that remain open during a task being executed.
+                 Called once at the end of a task for each virtual peer after the incoming
+                 queue has been exhausted. Only called once globally for a single task."))
 
 (extend-protocol Pipeline
   onyx.peer.IPipeline
