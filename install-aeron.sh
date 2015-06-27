7014d08991d040bff483e136ff49d091272a93cb
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-06-27 18:39:38 +0800

    Re-enable aeron in CI

--- install-aeron.sh
+++ install-aeron.sh
@@ -6,13 +6,13 @@ set -e
 if [ ! -e Agrona ]; then
   git clone https://github.com/real-logic/Agrona.git
   cd Agrona
-  git checkout 31d85ecfcbe1e3de38a0b5a095239d2ba973234b
+  git checkout bccda2335c728709ff6b8c6f23e89cc208d70e85
   ./gradlew
 fi
 
 if [ ! -e Aeron ]; then
   git clone https://github.com/real-logic/Aeron.git
   cd Aeron
-  git checkout e233ed01918421295506135e2851d5d2843d72c2
+  git checkout 95330cd20898b2055bf334e8a25a20879dbd345a
   ./gradlew
 fi
