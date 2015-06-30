99bfbd09c998ba1f1561aad724f7d75523ff094d
Michael Drogalis <mjd3089@rit.edu>
2015-06-30 08:10:48 -0700

    1.7 upgrade failing

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-Xmx4g"]
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.apache.curator/curator-framework "2.8.0"] 
                  [org.apache.curator/curator-test "2.8.0"]
