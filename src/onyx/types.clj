1f8e6afa3c075a91e1fbefa1ce94d528397d8f79
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-07-13 20:38:49 +0200

    Read vpeer-id separately to rest of buffer so that the buffer can be passed down to the peer that should perform the deserialization

--- src/onyx/types.clj
+++ src/onyx/types.clj
@@ -1,8 +1,16 @@
 (ns onyx.types)
 
+<<<<<<< HEAD
 (defrecord Message [id payload])
 
 (defrecord Leaf [message id acker-id completion-id ack-val hash-group route])
+=======
+(defrecord Leaf [message id acker-id completion-id ack-val ack-vals route routes hash-group])
+
+(defn leaf 
+  ([message] 
+     (->Leaf message nil nil nil nil nil nil nil nil)))
+>>>>>>> ba78b0e... Read vpeer-id separately to rest of buffer so that
 
 (defrecord Route [flow exclusions post-transformation action])
 
