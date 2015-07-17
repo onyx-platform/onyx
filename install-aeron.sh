b646b89e7f1f0b37e9b5439bcdeddf9499c3c930
Michael Drogalis <mjd3089@rit.edu>
2015-07-16 19:03:54 -0500

    Restore old deps to pass build.

---
+++ install-aeron.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ ! -e Agrona ]; then
+  git clone https://github.com/real-logic/Agrona.git
+  cd Agrona
+  git checkout bccda2335c728709ff6b8c6f23e89cc208d70e85
+  ./gradlew clean build install -x test
+fi
+
+if [ ! -e Aeron ]; then
+  git clone https://github.com/real-logic/Aeron.git
+  cd Aeron
+  git checkout 95330cd20898b2055bf334e8a25a20879dbd345a
+  ./gradlew clean build install -x test
+fi
