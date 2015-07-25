51a243c2ca856ebf04222fae8e700c6b7c94dd95
Michael Drogalis <mjd3089@rit.edu>
2015-07-25 13:26:05 -0700

    Read partition from local fs.
    [ci skip]

--- src/onyx/plugin/local_file.clj
+++ src/onyx/plugin/local_file.clj
@@ -4,15 +4,21 @@
             [taoensso.timbre :as timbre])
   (:import [java.io LineNumberReader FileReader]))
 
+(def partition-size 20)
+
 (defrecord LocalFile [location]
   p-ext/PipelineBatchInput
   (n-partitions [this]
     (let [lnr (LineNumberReader. (FileReader. (io/file location)))
           _ (.skip lnr Long/MAX_VALUE)
-          lines (inc (.getLineNumber lnr))
-          partition-size 20]
+          lines (inc (.getLineNumber lnr))]
       (.close lnr)
       (int (Math/ceil (/ 100 partition-size)))))
+
+  (read-partition [this part]
+    (with-open [rdr (clojure.java.io/reader location)]
+      (let [coll (line-seq rdr)]
+        (take partition-size (drop (* part partition-size) coll)))))
   
   p-ext/PipelineBatchOutput
   (write-content [this content]
