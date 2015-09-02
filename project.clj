d5fb83970749438602f74b2fd0e9191481c37b55
Michael Drogalis <mjd3089@rit.edu>
2015-09-02 12:30:04 -0400

    Release target.

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"snapshots" {:url "https://clojars.org/repo"
                               :username :env
-                              :password :env}}
+                              :password :env}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.apache.curator/curator-framework "2.8.0"]
