4b606ac44dfc4406e36568d41385e2b817c4b18f
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-10-02 13:01:34 +0800

    Add state extensions State extensions are for a log type to implement for storage and playback of state commands

---
+++ src/onyx/state/state_extensions.clj
@@ -0,0 +1,31 @@
+(ns onyx.state-extensions)
+
+(defmulti initialise-log 
+  (fn [log-type event]
+    log-type))
+
+(defmulti close-log 
+  (fn [log]
+    (type log)))
+
+;; For onyx to implement
+;; We can implement log storage for Kafka and maybe ZK (if small data is used and we gc)
+(defmulti store-log-entries 
+  "Store state update [op k v] entries in a log"
+  (fn [log event entries]
+    (type log)))
+
+(defmulti playback-log-entries 
+  "Read state update [op k v] entries from log"
+  (fn [log event state apply-fn]
+    (type log)))
+
+(defmulti store-seen-ids 
+  "Store seen ids in a log. Ideally these will be timestamped for auto expiry"
+  (fn [log event seen-ids]
+    (type log)))
+
+(defmulti playback-seen-ids 
+  "Read seen ids from a log"
+  (fn [log event bucket-state apply-fn]
+    (type log)))
