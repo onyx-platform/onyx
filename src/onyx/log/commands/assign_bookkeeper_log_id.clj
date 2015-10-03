a85f493efe685b5207fba3969b799f2b8a11e190
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-10-03 17:17:59 +0800

    Fix bookkeeper log command name

--- src/onyx/log/commands/assign_bookkeeper_log_id.clj
+++ src/onyx/log/commands/assign_bookkeeper_log_id.clj
@@ -7,21 +7,21 @@
             [onyx.schema :refer [Replica LogEntry Reactions ReplicaDiff State]]
             [onyx.extensions :as extensions]))
 
-(s/defmethod extensions/apply-log-entry :signal-ready :- Replica
+(s/defmethod extensions/apply-log-entry :assign-bookkeeper-log-id :- Replica
   [{:keys [args]} :- LogEntry replica]
   (update-in replica 
              [:state-logs (:job-id args) (:task-id args) (:slot-id args)]
              (fn [logs]
                (conj (vec logs) (:ledger-id args)))))
 
-(s/defmethod extensions/replica-diff :signal-ready :- ReplicaDiff
+(s/defmethod extensions/replica-diff :assign-bookkeeper-log-id :- ReplicaDiff
   [{:keys [args]} :- LogEntry old new]
   (second (diff (:state-logs old) (:state-logs new))))
 
-(s/defmethod extensions/reactions :signal-ready :- Reactions
+(s/defmethod extensions/reactions :assign-bookkeeper-log-id :- Reactions
   [{:keys [args]} :- LogEntry old new diff peer-args]
   [])
 
-(s/defmethod extensions/fire-side-effects! :signal-ready :- State
+(s/defmethod extensions/fire-side-effects! :assign-bookkeeper-log-id :- State
   [{:keys [args message-id]} :- LogEntry old new diff state]
   state)
