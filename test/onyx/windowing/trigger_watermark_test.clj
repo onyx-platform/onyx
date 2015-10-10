54fb4bf2b377b262e0911448979928be2977684b
Michael Drogalis <mjd3089@rit.edu>
2015-10-09 19:16:24 -0700

    Watermark test.

--- test/onyx/windowing/trigger_watermark_test.clj
+++ test/onyx/windowing/trigger_watermark_test.clj
@@ -1,13 +1,9 @@
-(ns onyx.windowing.trigger-punctuation-test
+(ns onyx.windowing.trigger-watermark-test
   (:require [clojure.test :refer [deftest is]]
             [onyx.triggers.triggers-api :as api]
             [onyx.api]))
 
-(def true-pred (constantly true))
-
-(def false-pred (constantly false))
-
-(deftest punctuation-true-pred
+(deftest watermark-test
   (let [window {:window/id :collect-segments
                 :window/task :identity
                 :window/type :fixed
@@ -16,16 +12,14 @@
                 :window/range [5 :minutes]}
         trigger {:trigger/window-id :collect-segments
                  :trigger/refinement :accumulating
-                 :trigger/on :punctuation
-                 :trigger/pred ::true-pred
+                 :trigger/on :watermark
                  :trigger/sync ::no-op
                  :trigger/id :trigger-id}
-        segment {}
-        event {:onyx.triggers/punctuation-preds {:trigger-id true-pred}}]
-    (is
-     (api/trigger-fire? event trigger {:window window :segment segment}))))
+        segment {:event-time (System/currentTimeMillis)}
+        event {}]
+    (is (api/trigger-fire? event trigger {:window window :segment segment
+                                          :upper-extent (dec (System/currentTimeMillis))})))
 
-(deftest punctuation-false-pred
   (let [window {:window/id :collect-segments
                 :window/task :identity
                 :window/type :fixed
@@ -34,11 +28,12 @@
                 :window/range [5 :minutes]}
         trigger {:trigger/window-id :collect-segments
                  :trigger/refinement :accumulating
-                 :trigger/on :punctuation
-                 :trigger/pred ::false-pred
+                 :trigger/on :watermark
                  :trigger/sync ::no-op
                  :trigger/id :trigger-id}
-        segment {}
-        event {:onyx.triggers/punctuation-preds {:trigger-id false-pred}}]
+        segment {:event-time (System/currentTimeMillis)}
+        event {}]
     (is
-     (not (api/trigger-fire? event trigger {:window window :segment segment})))))
+     (not
+      (api/trigger-fire? event trigger {:window window :segment segment
+                                        :upper-extent (inc (System/currentTimeMillis))})))))
