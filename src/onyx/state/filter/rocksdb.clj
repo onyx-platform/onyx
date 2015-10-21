7103b03902f106091c3a967c92e2e6807663c588
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-10-21 21:33:03 +0800

    Rename filter parameters. Fix filter restore via column families

--- src/onyx/state/filter/rocksdb.clj
+++ src/onyx/state/filter/rocksdb.clj
@@ -11,7 +11,7 @@
 
 (defrecord RocksDbInstance [dir db id-counter buckets bucket rotation-thread shutdown-ch])
 
-(defn build-bucket [^RocksDB db id]
+(defn build-bucket ^ColumnFamilyHandle [^RocksDB db id]
   (.createColumnFamily db 
                        (ColumnFamilyDescriptor. (.getBytes (str id)) 
                                                 (ColumnFamilyOptions.))))
@@ -19,15 +19,13 @@
 (defn clear-bucket! [db bucket]
   (.dropColumnFamily ^RocksDB db ^ColumnFamilyHandle bucket))
 
-(def max-buckets 10)
-
 (defn rotate-bucket! 
   "Rotates to the next bucket, and then starts the dropped one"
-  [db buckets bucket]
+  [db num-buckets buckets bucket]
   (let [new-bucket (build-bucket db (java.util.UUID/randomUUID))
         new-buckets (swap! buckets conj new-bucket)]
     (reset! bucket new-bucket)
-    (when (> (count new-buckets) max-buckets)
+    (when (> (count new-buckets) num-buckets)
       (swap! buckets (fn [bks] (vec (drop 1 bks))))
       (let [dropped-bucket (first new-buckets)]
         (clear-bucket! db dropped-bucket)))))
@@ -35,7 +33,8 @@
 (defn start-rotation-thread! [shutdown-ch peer-opts db id-counter buckets bucket]
   (thread
     (let [rotation-sleep (arg-or-default :onyx.rocksdb.filter/rotation-check-interval-ms peer-opts)
-          elements-per-bucket (arg-or-default :onyx.rocksdb.filter/rotate-filter-bucket-every-n peer-opts)] 
+          elements-per-bucket (arg-or-default :onyx.rocksdb.filter/num-ids-per-bucket peer-opts)
+          num-buckets (arg-or-default :onyx.rocksdb.filter/num-buckets peer-opts)] 
       (loop []
         (let [timeout-ch (timeout rotation-sleep)
               ch (second (alts!! [timeout-ch shutdown-ch]))]
@@ -43,7 +42,7 @@
             (try
               (when (> @id-counter elements-per-bucket)
                 (info "Rotating filter bucket after" elements-per-bucket "elements.")
-                (rotate-bucket! db buckets bucket)
+                (rotate-bucket! db buckets bucket num-buckets)
                 (reset! id-counter 0))
               (catch Throwable e
                 (fatal e)))
@@ -77,7 +76,6 @@
         rotation-thread (start-rotation-thread! shutdown-ch peer-opts db id-counter buckets bucket)]
     (->RocksDbInstance db-dir db id-counter buckets bucket rotation-thread shutdown-ch)))
 
-
 (def magic-value 
   (doto (byte-array 1)
     (aset 0 (byte 99))))
@@ -107,35 +105,48 @@
   (.close ^RocksDB (:db rocks-db))
   (FileUtils/deleteDirectory (java.io.File. ^String (:dir rocks-db))))
 
+(defn clear-buckets! [{:keys [db bucket buckets] :as rocks-db}]
+  (run! (partial clear-bucket! db) @buckets)
+  (reset! buckets []))
+
+(defn add-bucket! [{:keys [db bucket buckets] :as rocks-db}
+                   bucket-values]
+  (let [new-bucket (build-bucket db (java.util.UUID/randomUUID))] 
+    (reset! bucket new-bucket)
+    (swap! buckets conj new-bucket)
+    (run! (fn [[k v]]
+            (.put ^RocksDB db new-bucket ^bytes k ^bytes v))
+          bucket-values)))
+
 (defmethod state-extensions/restore-filter onyx.state.filter.rocksdb.RocksDbInstance 
-  [{:keys [db bucket id-counter] :as rocks-db} event snapshot]
+  [{:keys [db bucket buckets id-counter] :as rocks-db} event snapshot]
+  (clear-buckets! rocks-db)
   (reset! id-counter (:id-counter snapshot))
-  (reset! bucket (:bucket snapshot))
-  (run! (fn [[k v]]
-          (.put ^RocksDB db ^bytes k ^bytes v)) 
-        (:kvs snapshot))
+  (run! #(add-bucket! rocks-db %) (:buckets snapshot))
   rocks-db)
 
 (defmethod state-extensions/snapshot-filter onyx.state.filter.rocksdb.RocksDbInstance 
   [filter-state _] 
   (let [db ^RocksDB (:db filter-state)
         snapshot ^Snapshot (.getSnapshot db)
-        bucket @(:bucket filter-state)
+        buckets @(:buckets filter-state)
         id-counter @(:id-counter filter-state)
         read-options ^ReadOptions (doto (ReadOptions.)
                                     (.setSnapshot snapshot))]
     (future 
-      (let [iterator ^RocksIterator (.newIterator db read-options)]
-        (try
-          (.seekToFirst iterator)
-          {:bucket bucket 
-           :id-counter id-counter
-           :kvs (loop [ids (list)]
-                  (if (.isValid iterator)
-                    (let [id (list (.key iterator) (.value iterator))] 
-                      (.next iterator)
-                      (recur (conj ids id)))
-                    ids))}
-          (finally
-            (.releaseSnapshot db (.snapshot read-options))
-            (.dispose iterator)))))))
+      (try {:id-counter id-counter 
+       :buckets (mapv (fn [^ColumnFamilyHandle bucket]
+                        (let [iterator ^RocksIterator (.newIterator db bucket read-options)]
+                          (try
+                            (.seekToFirst iterator)
+                            (loop [ids (list)]
+                              (if (.isValid iterator)
+                                (let [id (list (.key iterator) (.value iterator))] 
+                                  (.next iterator)
+                                  (recur (conj ids id)))
+                                ids))
+                            (finally
+                              (.dispose iterator)))))
+                      buckets)}
+           (finally
+             (.releaseSnapshot db (.snapshot read-options)))))))
--- test/onyx/state/filter/rocksdb_test.clj
+++ test/onyx/state/filter/rocksdb_test.clj
@@ -22,8 +22,8 @@
                    vs'))))
       vs)))
 
-(defn write-bucket [per-bucket f bucket]
-  (rdb/rotate-bucket! (:db f) (:buckets f) (:bucket f))
+(defn write-bucket [num-buckets per-bucket f bucket]
+  (rdb/rotate-bucket! (:db f) num-buckets (:buckets f) (:bucket f))
   (reduce (fn [f' vr]
             (se/apply-filter-id f' {} (+ vr (* bucket per-bucket))))
           f
@@ -32,13 +32,14 @@
 (deftest rocksdb-filter-test 
   (with-redefs [rdb/start-rotation-thread! (fn [_ _ _ _ _ _] (thread))] 
     (let [per-bucket 10
-          rfilter (se/initialize-filter :rocksdb {:onyx.core/peer-opts {:onyx.rocksdb.filter/rotate-filter-bucket-every-n per-bucket}
+          n-buckets 255
+          rfilter (se/initialize-filter :rocksdb {:onyx.core/peer-opts {:onyx.rocksdb.filter/num-ids-per-bucket per-bucket
+                                                                        :onyx.rocksdb.filter/num-buckets n-buckets}
                                                   :onyx.core/id (str :peer-id (java.util.UUID/randomUUID))
                                                   :onyx.core/task-id :task-id})
-          n-buckets 255
           filter-range (range (inc n-buckets))]
       (try
-        (let [new-rfilter (reduce (partial write-bucket per-bucket)
+        (let [new-rfilter (reduce (partial write-bucket n-buckets per-bucket)
                                   rfilter
                                   (range n-buckets))]
 
@@ -47,7 +48,7 @@
           (is (= (range 2550)
                  (sort (extract-values (:db new-rfilter) @(:buckets new-rfilter)))))
 
-          (write-bucket per-bucket new-rfilter 255)
+          (write-bucket n-buckets per-bucket new-rfilter 255)
 
           (testing "Initial bucket rotated, some data deleted and new data entered"
             (is (= (range per-bucket (+ 2550 per-bucket))
@@ -55,16 +56,17 @@
         (finally
           (se/close-filter rfilter {}))))))
 
-#_(deftest rocksdb-restore-test 
+(deftest rocksdb-restore-test 
   (with-redefs [rdb/start-rotation-thread! (fn [_ _ _ _ _ _] (thread))] 
     (let [per-bucket 10
-          rfilter (se/initialize-filter :rocksdb {:onyx.core/peer-opts {:onyx.rocksdb.filter/rotate-filter-bucket-every-n per-bucket}
+          n-buckets 255
+          rfilter (se/initialize-filter :rocksdb {:onyx.core/peer-opts {:onyx.rocksdb.filter/num-ids-per-bucket per-bucket
+                                                                        :onyx.rocksdb.filter/num-buckets n-buckets}
                                                   :onyx.core/id (str :peer-id (java.util.UUID/randomUUID))
                                                   :onyx.core/task-id :task-id})
-          n-buckets 255
           filter-range (range (inc n-buckets))]
       (try
-        (let [new-rfilter (reduce (partial write-bucket per-bucket)
+        (let [new-rfilter (reduce (partial write-bucket n-buckets per-bucket)
                                   rfilter
                                   (range n-buckets))
 
@@ -75,10 +77,10 @@
                                                         :onyx.core/task-id :task-id})
                                  (se/restore-filter {} snapshot))]
           (try 
-            (is (= (extract-values (:db new-rfilter))
-                   (extract-values (:db restore-filter))))
-            (is (= @(:bucket new-rfilter)
-                   @(:bucket restore-filter)))
+            (is (= (extract-values (:db new-rfilter) @(:buckets new-rfilter))
+                   (extract-values (:db restore-filter) @(:buckets restore-filter))))
+            (is (= (count @(:buckets new-rfilter))
+                   (count @(:buckets restore-filter))))
             (is (= @(:id-counter new-rfilter)
                    @(:id-counter restore-filter)))
             (finally
