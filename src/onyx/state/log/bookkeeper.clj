f8669da22933ced145e0d7bc893cd8fca87ea944
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-10-22 01:51:03 +0800

    swap-pair! no longer required by bookkeeper impl

--- src/onyx/state/log/bookkeeper.clj
+++ src/onyx/state/log/bookkeeper.clj
@@ -49,7 +49,6 @@
 (defn password [peer-opts]
   (.getBytes ^String (arg-or-default :onyx.bookkeeper/ledger-password peer-opts)))
 
-
 (defn new-ledger ^LedgerHandle [client peer-opts]
   (let [ensemble-size (arg-or-default :onyx.bookkeeper/ledger-ensemble-size peer-opts)
         quorum-size (arg-or-default :onyx.bookkeeper/ledger-quorum-size peer-opts)]
@@ -60,20 +59,6 @@
   (reify AsyncCallback$AddCallback
     (addComplete [this rc lh entry-id callback-fn]
       (callback-fn))))
-
-;; Adapted from Prismatic Plumbing:
-;; https://github.com/Prismatic/plumbing/blob/c53ba5d0adf92ec1e25c9ab3b545434f47bc4156/src/plumbing/core.cljx#L346-L361
-(defn swap-pair!
-  "Like swap! but returns a pair [old-val new-val]"
-  ([a f]
-     (loop []
-       (let [old-val @a
-             new-val (f old-val)]
-         (if (compare-and-set! a old-val new-val)
-           [old-val new-val]
-           (recur)))))
-  ([a f & args]
-     (swap-pair! a #(apply f % args))))
 
 (defn compaction-transition 
   "Transitions to a new compacted ledger, plus a newly created ledger created
