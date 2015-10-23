8338a7374d0db515339f0c69511a18315addf3f7
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-10-23 18:08:03 +0800

    WIP, statefully check BookKeeper log impl

--- test/onyx/state/log/bookkeeper_stateful_check.clj
+++ test/onyx/state/log/bookkeeper_stateful_check.clj
@@ -1,6 +1,7 @@
 (ns onyx.state.log.bookkeeper-stateful-check
-  (:require [stateful-check.core :refer [reality-matches-model print-test-results]]
+  (:require [stateful-check.core :refer [reality-matches-model print-test-results specification-correct?]]
             [onyx.log.generators :as loggen]
+            [taoensso.timbre :refer [info error warn trace fatal] :as timbre]
             [onyx.extensions :as ext]
             [onyx.state.state-extensions :as state-ext]
             [onyx.messaging.dummy-messenger :refer [dummy-messenger]]
@@ -11,72 +12,98 @@
             [clojure.test.check.clojure-test :refer [defspec]]
             [onyx.test-helper :refer [load-config]]           
             [clojure.test :refer :all]
+            [onyx.state.log.bookkeeper :as log-bk]
             [com.gfredericks.test.chuck :refer [times]]
             [com.gfredericks.test.chuck.clojure-test :refer [checking]]))
 
-(comment
 (defn new-replica []
-  (atom {:replica {:job-scheduler :onyx.job-scheduler/balanced
-                   :messaging {:onyx.messaging/impl :dummy-messenger}}
-         :message-id 0}))
+    (atom {:replica {:job-scheduler :onyx.job-scheduler/balanced
+                     :messaging {:onyx.messaging/impl :dummy-messenger}}
+           :message-id 0}))
+
 
 (def messenger (dummy-messenger {:onyx.peer/try-join-once? false}))
 
-(def env 
-  (let [id (java.util.UUID/randomUUID)
-        config (load-config)
-        env-config (assoc (:env-config config) :onyx/id id)
-        peer-config (assoc (:peer-config config) 
-                           :onyx/id id
-                           :onyx.bookkeeper/write-batch-size 1
-                           :onyx.bookkeeper/write-batch-timeout 10000000)
-        env (onyx.api/start-env env-config)]
+(def id (java.util.UUID/randomUUID))
 
-    (try
-      
-      
-(def new-peer-spec
-  {:next-state (fn [state args result]
-                 (let [pipeline {}] 
-                   {:event (assoc pipeline 
-                                  :onyx.core/window-state 
-                                  (->WindowState (state-ext/initialize-filter :set pipeline) 
-                                                 {}))}))
-   :real/command #'new-replica})
+(def config (load-config))
 
+(def env-config (assoc (:env-config config) :onyx/id id))
 
-(def new-log-entry
+(def peer-config (assoc (:peer-config config) 
+                        :onyx/id id
+                        :onyx.bookkeeper/write-batch-size 1
+                        :onyx.bookkeeper/write-batch-timeout 10000000))
+
+(def initial-bookkeeper-state {:logs [[]] :ledger-ids [0]})
+
+(def bookkeeper-state (atom initial-bookkeeper-state))
+(def bookkeeper-peer-state (atom nil))
+
+(defn add-to-bookkeeper [arg]
+  (swap! bookkeeper-state (fn [bs-state]
+                            (update-in bs-state [:logs (last (:ledger-ids bs-state))] conj arg))))
+
+(def add-to-log-espec
   {:model/args (fn [state]
-                 (gen/tuple
-                   (gen/return (:replica-real state))
-                   (gen/elements (:peers state))))
-   :model/precondition (fn [state _]
-                         (not-empty (:peers state)))
-   :real/postcondition (fn [state next-state args updated-real]
-                         (= (count (:peers (:replica updated-real)))
-                            (count (:peers next-state))))
-   :real/command #'apply-leave-entry
-   :next-state (fn [state [_ peer-id] _]
-                 (update-in state [:peers] disj peer-id))})
+                 [gen/int])
+   :next-state (fn [state [arg] result]
+                 (conj state arg))
+   :real/postcondition (fn [prev-state next-state [arg] result]
+                         (= (reduce into [] (:logs @bookkeeper-state)) next-state))
+   :real/command #'add-to-bookkeeper})
 
-(def replica-spec
-  {:commands {:new-peer new-peer-spec
-              :new-log-entry new-log-entry
-              :add-peer add-peer-spec
-              :remove-peer remove-peer-spec}
-   :model/generate-command (fn [state]
-                             (cond (nil? state)
-                                   (gen/return :new-replica)
-                                   (not-empty (:peers state))
-                                   (gen/elements [:add-peer :remove-peer])
-                                   :else
-                                   (gen/elements [:add-peer])))})
+(defn switch-to-next-ledger []
+  (swap! bookkeeper-state (fn [bs-state]
+                            (-> bs-state
+                                (update :logs conj [])
+                                (update :ledger-ids conj (inc (apply max (:ledger-ids bs-state))))))))
+
+(def peer-crash-new-log-spec
+  {:model/args (fn [state]
+                 [])
+   :next-state (fn [state [arg] result]
+                 state)
+   ;:real/postcondition (fn [prev-state next-state [arg] result]
+   ;                      (= (reduce into [] (:logs @bookkeeper-state)) next-state))
+   :real/command #'switch-to-next-ledger})
+
+(defn new-peer-log []
+  (let [bk-peer-val {:ledger-ids []
+                     :event-log []
+                     :event (let [pipeline {:onyx.core/peer-opts peer-config}] 
+                              (-> pipeline
+                                  ;(assoc :onyx.core/window-state (->WindowState (state-ext/initialize-filter :set pipeline) {}))
+                                  (assoc :onyx.core/state-log (state-ext/initialize-log :bookkeeper pipeline))))}] 
+    (reset! bookkeeper-state initial-bookkeeper-state)
+    (reset! bookkeeper-peer-state bk-peer-val)))
+
+(defn close-peer-log [state]
+  (let [real-state-event (:event @bookkeeper-peer-state)] 
+    (info " Closing log")
+    (state-ext/close-log (:onyx.core/state-log real-state-event) real-state-event)))
+
+(def log-spec
+  {:commands {:add-to-log add-to-log-espec
+              :peer-crash-new-log peer-crash-new-log-spec}
+   :real/setup #'new-peer-log 
+   :real/cleanup #'close-peer-log
+   :real/postcondition (fn [final-state]
+                         (= (reduce into [] (:logs @bookkeeper-state)) final-state))
+   :initial-state (fn [setup] [])
+   ;:model/generate-command (fn [state]
+   ;                          (gen/elements [(gen/return :add-to-log)
+   ;                                         (gen/return :peer-crash-new-log)]))
+   })
 
 
-#_(defspec run-replica-spec 5 (reality-matches-model? replica-spec))
-#_(print-test-results replica-spec (quick-check 50 (reality-matches-model? replica-spec) :seed 1417059242645))
-
-
-
-(finally
-  (onyx.api/shutdown-env env))))))
+(deftest log-test-correct
+  (with-redefs [log-bk/assign-bookkeeper-log-id-spin (fn [event new-ledger-id]
+                                                       (swap! bookkeeper-peer-state 
+                                                              (fn [ps]
+                                                                (update ps :ledger-ids conj new-ledger-id))))]
+    (let [env (onyx.api/start-env env-config)]
+      (try
+        (is (specification-correct? log-spec))
+        (finally
+          (onyx.api/shutdown-env env))))))
