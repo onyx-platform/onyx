50d8b8acc294959a8bdfb545eb4b05554abf9325
Michael Drogalis <mjd3089@rit.edu>
2015-10-30 18:34:27 -0700

    Allow singular unit usage.

--- src/onyx/schema.clj
+++ src/onyx/schema.clj
@@ -173,10 +173,11 @@
   (s/enum :accumulating :discarding))
 
 (def TriggerPeriod 
-  (s/enum :milliseconds :seconds :minutes :hours :days))
+  (s/enum :milliseconds :seconds :minutes :hours :days
+          :millisecond :second :minute :hour :day))
 
 (def TriggerThreshold
-  (s/enum :elements))
+  (s/enum :elements :element))
 
 (def Trigger
   {:trigger/window-id s/Keyword
