923e8d5292f8fbd7478b1a032b1b772d3fd41969
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-25 15:00:24 +0800

    Liveness timeout property should be a string

--- src/onyx/messaging/aeron.clj
+++ src/onyx/messaging/aeron.clj
@@ -187,7 +187,7 @@
     (let [embedded-driver? (arg-or-default :onyx.messaging.aeron/embedded-driver? opts)
           inter-service-timeout (arg-or-default :onyx.messaging.aeron/inter-service-timeout-ns opts)
           ;; client liveness timeout should match inter-service-timeout
-          _ (System/setProperty "aeron.client.liveness.timeout" inter-service-timeout)
+          _ (System/setProperty "aeron.client.liveness.timeout" (str inter-service-timeout))
           media-driver-context (if embedded-driver?
                                  (doto (MediaDriver$Context.)))
           media-driver (if embedded-driver?
