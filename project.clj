ce4ba4d37522af55d73292865143b64cd3436c8f
Michael Drogalis <mjd3089@rit.edu>
2015-11-26 12:12:09 -0800

    Update to Aeron 0.2.2.

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
                  [com.stuartsierra/component "0.3.0"]
                  [com.taoensso/timbre "4.1.4"]
                  [com.taoensso/nippy "2.10.0"]
-                 [uk.co.real-logic/aeron-all "0.2.1"]
+                 [uk.co.real-logic/aeron-all "0.2.2"]
                  [prismatic/schema "1.0.3"]
                  [org.apache.zookeeper/zookeeper "3.4.6" 
                   :exclusions [org.slf4j/slf4j-log4j12]]
