f1abd82b33a3cd4f2e64d7fe08439d6bb9ae90fe
Michael Drogalis <mjd3089@rit.edu>
2015-12-18 19:58:47 -0800

    Fix ns from moved test.

--- test/onyx/flow_conditions/flow_pred_test.clj
+++ test/onyx/flow_conditions/flow_pred_test.clj
@@ -6,49 +6,39 @@
 (def false-pred (constantly false))
 
 (deftest flow-pred
-  (is (true? ((build-pred-fn :onyx.flow-pred-test/true-pred {}) [1 2 3])))
+  (is (true? ((build-pred-fn ::true-pred {}) [1 2 3])))
 
-  (is (false? ((build-pred-fn [:and :onyx.flow-pred-test/true-pred
-                               :onyx.flow-pred-test/false-pred] {}) [5 6 7])))
+  (is (false? ((build-pred-fn [:and ::true-pred ::false-pred] {}) [5 6 7])))
 
-  (is (true? ((build-pred-fn [:and :onyx.flow-pred-test/true-pred
-                              :onyx.flow-pred-test/true-pred] {}) [5 6 7])))
+  (is (true? ((build-pred-fn [:and ::true-pred ::true-pred] {}) [5 6 7])))
 
-  (is (false? ((build-pred-fn [:and :onyx.flow-pred-test/false-pred
-                               [:and :onyx.flow-pred-test/true-pred
-                                :onyx.flow-pred-test/true-pred]] {}) [5])))
+  (is (false? ((build-pred-fn [:and ::false-pred
+                               [:and ::true-pred ::true-pred]] {}) [5])))
 
-  (is (false? ((build-pred-fn [:and :onyx.flow-pred-test/true-pred
-                               [:and :onyx.flow-pred-test/true-pred
-                                :onyx.flow-pred-test/false-pred]] {}) [5])))
+  (is (false? ((build-pred-fn [:and ::true-pred
+                               [:and ::true-pred ::false-pred]] {}) [5])))
 
-  (is (true? ((build-pred-fn [:or :onyx.flow-pred-test/true-pred
-                              :onyx.flow-pred-test/false-pred] {}) [5])))
+  (is (true? ((build-pred-fn [:or ::true-pred ::false-pred] {}) [5])))
 
-  (is (nil? ((build-pred-fn [:or :onyx.flow-pred-test/false-pred
-                             :onyx.flow-pred-test/false-pred] {}) [5])))
+  (is (nil? ((build-pred-fn [:or ::false-pred ::false-pred] {}) [5])))
 
-  (is (true? ((build-pred-fn [:or :onyx.flow-pred-test/true-pred
-                              [:or :onyx.flow-pred-test/true-pred
-                               :onyx.flow-pred-test/false-pred]] {}) [5])))
+  (is (true? ((build-pred-fn [:or ::true-pred
+                              [:or ::true-pred ::false-pred]] {}) [5])))
 
-  (is (false? ((build-pred-fn [:not :onyx.flow-pred-test/true-pred] {}) [1 2 3])))
+  (is (false? ((build-pred-fn [:not ::true-pred] {}) [1 2 3])))
 
-  (is (true? ((build-pred-fn [:not :onyx.flow-pred-test/false-pred] {}) [5])))
+  (is (true? ((build-pred-fn [:not ::false-pred] {}) [5])))
 
-  (is (false? ((build-pred-fn [:not [:not :onyx.flow-pred-test/false-pred]] {}) [5])))
+  (is (false? ((build-pred-fn [:not [:not ::false-pred]] {}) [5])))
 
-  (is (true? ((build-pred-fn [:not [:not :onyx.flow-pred-test/true-pred]] {}) [5])))
+  (is (true? ((build-pred-fn [:not [:not ::true-pred]] {}) [5])))
 
-  (is (true? ((build-pred-fn [:or :onyx.flow-pred-test/true-pred
-                              [:and :onyx.flow-pred-test/false-pred
-                               :onyx.flow-pred-test/false-pred]] {}) [5])))
+  (is (true? ((build-pred-fn [:or ::true-pred
+                              [:and ::false-pred ::false-pred]] {}) [5])))
 
-  (is (nil? ((build-pred-fn [:or [:not :onyx.flow-pred-test/true-pred]
-                             [:and :onyx.flow-pred-test/false-pred
-                              :onyx.flow-pred-test/false-pred]] {}) [5])))
+  (is (nil? ((build-pred-fn [:or [:not ::true-pred]
+                             [:and ::false-pred ::false-pred]] {}) [5])))
 
-  (is (true? ((build-pred-fn [:and :onyx.flow-pred-test/true-pred
-                              [:and :onyx.flow-pred-test/true-pred
-                               :onyx.flow-pred-test/true-pred]] {}) [1 2 3])))) 
+  (is (true? ((build-pred-fn [:and ::true-pred
+                              [:and ::true-pred ::true-pred]] {}) [1 2 3]))))
 
