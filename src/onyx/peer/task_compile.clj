349b448492cd414170946cb82e4de4114678668e
Michael Drogalis <mjd3089@rit.edu>
2015-12-19 21:21:16 -0800

    Better names for flow compilation functions.

--- src/onyx/peer/task_compile.clj
+++ src/onyx/peer/task_compile.clj
@@ -19,10 +19,10 @@
        (assoc condition :flow/predicate (build-pred-fn (:flow/predicate condition) condition)))
      conditions)))
 
-(defn compile-fc-norms [flow-conditions task-name]
+(defn compile-fc-happy-path [flow-conditions task-name]
   (compile-flow-conditions flow-conditions task-name (comp not :flow/thrown-exception?)))
 
-(defn compile-fc-exs [flow-conditions task-name]
+(defn compile-fc-exception-path [flow-conditions task-name]
   (compile-flow-conditions flow-conditions task-name :flow/thrown-exception?))
 
 (defn resolve-lifecycle-calls [calls]
@@ -212,9 +212,9 @@
 (defn flow-conditions->event-map [event flow-conditions task-name]
   (-> event
       (assoc :onyx.core/compiled-norm-fcs
-             (compile-fc-norms flow-conditions task-name))
+             (compile-fc-happy-path flow-conditions task-name))
       (assoc :onyx.core/compiled-ex-fcs
-             (compile-fc-exs flow-conditions task-name))))
+             (compile-fc-exception-path flow-conditions task-name))))
 
 (defn lifecycles->event-map [event lifecycles task-name]
   (-> event
