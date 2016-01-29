03564984b80cf002428366a9442b9fd6acd85064
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-30 00:02:39 +0800

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx "0.8.7"
+(defproject org.onyxplatform/onyx "0.8.8-SNAPSHOT"
   :description "Distributed, masterless, high performance, fault tolerant data processing for Clojure"
   :url "https://github.com/onyx-platform/onyx"
   :license {:name "Eclipse Public License"
