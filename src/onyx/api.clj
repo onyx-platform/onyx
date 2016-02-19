72d1770822370c25e8230fb68de0fe711554a6a5
Tienson Qin <tiensonqin@gmail.com>
2016-02-19 11:48:06 +0800

    No need to assoc workflow to job. Since every job should have the key :workflow.

--- src/onyx/api.clj
+++ src/onyx/api.clj
@@ -152,7 +152,7 @@
    (submit-job peer-config job {:monitoring :no-op}))
   ([peer-config job monitoring-config]
    (try (validator/validate-peer-config peer-config)
-        (validator/validate-job (assoc job :workflow (:workflow job)))
+        (validator/validate-job job)
         (validator/validate-flow-conditions (:flow-conditions job) (:workflow job))
         (validator/validate-lifecycles (:lifecycles job) (:catalog job))
         (validator/validate-windows (:windows job) (:catalog job))
