cb2667a7f818615c2c3f68388e39e8699e868d96
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-17 00:14:05 +0800

    Fix logged stacktrace

--- src/onyx/static/logging.clj
+++ src/onyx/static/logging.clj
@@ -26,7 +26,7 @@
                      ;; (str (type ...)) prefixes "class" in the string.
                      (.getName ^java.lang.Class (.getClass ^clojure.lang.ExceptionInfo e))
                      (.getMessage ^clojure.lang.ExceptionInfo e))]
-     (ex-info msg error-keys (.getCause ^clojure.lang.ExceptionInfo e)))))
+     (ex-info msg error-keys e))))
 
 (defn exception-msg [task-information msg]
   (str (log-prefix task-information) msg))
