f826bdc51d8420a2456a6df23020224c996a1712
Michael Drogalis <mjd3089@rit.edu>
2016-03-18 09:20:46 -0700

    Release version 0.9.0-beta2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx "0.9.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx "0.9.0-beta2"
   :description "Distributed, masterless, high performance, fault tolerant data processing for Clojure"
   :url "https://github.com/onyx-platform/onyx"
   :license {:name "Eclipse Public License"
