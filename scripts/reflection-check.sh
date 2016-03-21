e8dd9b2553a0d0e6229aa7545a9ab71e65517c8c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-21 22:30:51 +0800

    Fix reflection check log dir

--- scripts/reflection-check.sh
+++ scripts/reflection-check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ARTIFACT_DIR=$CIRCLE_BUILD_NUM/$CIRCLE_NODE_INDEX/$BR"_"$1
+
 grep -v _test log_artifact/$ARTIFACT_DIR/stderrout.log | grep onyx | grep Reflection
 
 if [ $? -eq 1 ]; then
