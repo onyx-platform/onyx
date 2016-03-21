ef9cc15b13a9007facfdac9c39c0657351b75cc9
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-21 20:26:18 +0800

    Check logs for reflection

--- scripts/reflection-check.sh
+++ scripts/reflection-check.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env zsh
 
-# Ok to reflect in tests
-# Try not to worry about upstream libraries for now
-# Only care about reflection, not boxed math for now
-lein with-profile dev,reflection-check,circle-ci,clojure-1.8 test :smoke |& 
-grep -v _test | 
+grep -v _test log_artifact/$ARTIFACT_DIR/stderrout.log | 
 grep onyx |
 grep Reflection
 
