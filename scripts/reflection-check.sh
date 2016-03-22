5dabf13157f0795039fe23e12a31884dc4159772
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-22 18:54:14 +0800

    Proper reflection check

--- scripts/reflection-check.sh
+++ scripts/reflection-check.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "$CIRCLE_BRANCH" ]; then
-	export BR=$CI_BRANCH
-else
-	export BR=$CIRCLE_BRANCH
-fi
-
-ARTIFACT_DIR=$CIRCLE_BUILD_NUM/$CIRCLE_NODE_INDEX/$BR"_"$1
-
-grep -v _test log_artifact/$ARTIFACT_DIR/stderrout.log | grep onyx | grep Reflection
+grep -v _test stderrout.log | grep onyx | grep Reflection
 
 if [ $? -eq 1 ]; then
   exit 0
