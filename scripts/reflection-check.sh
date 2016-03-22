7d5bff6b62363d1a33550e15423f00b35bf84113
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-22 18:44:48 +0800

    Fix output lookup

--- scripts/reflection-check.sh
+++ scripts/reflection-check.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$CIRCLE_BRANCH" ]; then
+	export BR=$CI_BRANCH
+else
+	export BR=$CIRCLE_BRANCH
+fi
+
 ARTIFACT_DIR=$CIRCLE_BUILD_NUM/$CIRCLE_NODE_INDEX/$BR"_"$1
 
 grep -v _test log_artifact/$ARTIFACT_DIR/stderrout.log | grep onyx | grep Reflection
