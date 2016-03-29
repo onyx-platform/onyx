4b69755461f5e0816d8d9748ee2afae4a9228452
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-29 18:54:40 +0800

    Pipeline ext not required in validation test

--- test/onyx/validation_test.clj
+++ test/onyx/validation_test.clj
@@ -1,6 +1,5 @@
 (ns onyx.validation-test
-  (:require [onyx.peer.pipeline-extensions :as p-ext]
-            [onyx.test-helper :refer [load-config with-test-env]]
+  (:require [onyx.test-helper :refer [load-config with-test-env]]
             [taoensso.timbre :refer [info] :as timbre]
             [clojure.test :refer [deftest is testing]]
             [onyx.schema :as os]
