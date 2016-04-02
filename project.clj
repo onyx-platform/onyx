ef9b9a80454694bb418e57a5b9961d8e885b2ddf
Ole Krüger <ole@kru.gr>
2016-04-02 19:42:14 +0200

    Update to aeron 0.9.5

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [com.stuartsierra/component "0.3.1"]
                  [com.taoensso/timbre "4.1.4"]
                  [com.taoensso/nippy "2.10.0"]
-                 [uk.co.real-logic/aeron-all "0.9.1"]
+                 [io.aeron/aeron-all "0.9.5"]
                  [prismatic/schema "1.0.5"]
                  [log4j/log4j "1.2.17"]
                  [clj-tuple "0.2.2"]]
