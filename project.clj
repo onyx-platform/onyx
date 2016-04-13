a82da95b736cc16f14dbc2f293293b13c86e453a
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-04-13 18:15:57 +0800

    Release version 0.9.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx "0.9.4-SNAPSHOT"
+(defproject org.onyxplatform/onyx "0.9.4"
   :description "Distributed, masterless, high performance, fault tolerant data processing for Clojure"
   :url "https://github.com/onyx-platform/onyx"
   :license {:name "Eclipse Public License"
