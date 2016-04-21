9aa597d10255ce1c53f129d6f96be6f9c259d9d7
Bridget Hillyer <bridget.hillyer@gmail.com>
2016-04-21 15:53:54 -0400

    Show friendlier error message for schema validation
    * we are already doing so for catalog, add to peer-client-config,
    flow conditions, lifecycles, windows, and triggers

--- src/onyx/api.clj
+++ src/onyx/api.clj
@@ -152,11 +152,11 @@
    (submit-job peer-client-config job {:monitoring :no-op}))
   ([peer-client-config job monitoring-config]
    (try (validator/validate-peer-client-config peer-client-config)
-        (validator/validate-job job)
         (validator/validate-flow-conditions (:flow-conditions job) (:workflow job))
         (validator/validate-lifecycles (:lifecycles job) (:catalog job))
         (validator/validate-windows (:windows job) (:catalog job))
         (validator/validate-triggers (:triggers job) (:windows job))
+        (validator/validate-job job)
         (catch Throwable t
           (error t)
           (throw t)))
