e07754609ab65a9ffbfc6adcfc60c9f49927a825
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-05-09 23:22:53 +0800

    Add functionality to reduce test boiler plate

--- test/onyx/peer/abs_plugin_test.clj
+++ test/onyx/peer/abs_plugin_test.clj
@@ -1,75 +1,28 @@
 (ns onyx.peer.abs-plugin-test
-  (:require [clojure.core.async :refer [chan >!! <!! close! sliding-buffer]]
-            [clojure.test :refer [deftest is testing]]
-            [onyx.plugin.core-async :refer [take-segments!]]
+  (:require [clojure.test :refer [deftest is testing]]
+            [onyx.test-boilerplate :refer [build-job run-test-job]]
             [onyx.test-helper :refer [load-config with-test-env add-test-env-peers!]]
             [onyx.api]))
-
-(def n-messages 100)
-
-(def in-chan (atom nil))
-
-(def out-chan (atom nil))
-
-(defn inject-in-ch [event lifecycle]
-  {:core.async/chan @in-chan})
-
-(defn inject-out-ch [event lifecycle]
-  {:core.async/chan @out-chan})
-
-(def in-calls
-  {:lifecycle/before-task-start inject-in-ch})
-
-(def out-calls
-  {:lifecycle/before-task-start inject-out-ch})
 
 (defn my-inc [{:keys [n] :as segment}]
   (assoc segment :n (inc n)))
 
 (deftest abs-plugin-test
-  (let [id (java.util.UUID/randomUUID)
-        config (load-config)
-        env-config (assoc (:env-config config) :onyx/tenancy-id id)
-        peer-config (assoc (:peer-config config) :onyx/tenancy-id id)]
-    (with-test-env [test-env [3 env-config peer-config]]
-      (let [batch-size 20
-            catalog [{:onyx/name :in
-                      :onyx/plugin :onyx.plugin.core-async/input
-                      :onyx/type :input
-                      :onyx/medium :core.async
-                      :onyx/batch-size batch-size
-                      :onyx/max-peers 1
-                      :onyx/doc "Reads segments from a core.async channel"}
-
-                     {:onyx/name :inc
-                      :onyx/fn ::my-inc
-                      :onyx/type :function
-                      :onyx/batch-size batch-size}
-
-                     {:onyx/name :out
-                      :onyx/plugin :onyx.plugin.core-async/output
-                      :onyx/type :output
-                      :onyx/medium :core.async
-                      :onyx/batch-size batch-size
-                      :onyx/max-peers 1
-                      :onyx/doc "Writes segments to a core.async channel"}]
-            workflow [[:in :inc] [:inc :out]]
-            lifecycles [{:lifecycle/task :in
-                         :lifecycle/calls ::in-calls}
-                        
-                        {:lifecycle/task :out
-                         :lifecycle/calls ::out-calls}]
-            _ (reset! in-chan (chan (inc n-messages)))
-            _ (reset! out-chan (chan (sliding-buffer (inc n-messages))))
-            _ (doseq [n (range n-messages)]
-                (>!! @in-chan {:n n}))
-            _ (close! @in-chan)
-            _ (onyx.api/submit-job peer-config
-                                   {:catalog catalog
-                                    :workflow workflow
-                                    :lifecycles lifecycles
-                                    :task-scheduler :onyx.task-scheduler/balanced})
-            results (take-segments! @out-chan)]
-
-        (let [expected (set (map (fn [x] {:n (inc x)}) (range n-messages)))]
-          (is (= expected (set (butlast results)))))))))
+  (let [n-messages 100
+        task-opts {:onyx/batch-size 20}
+        job (build-job [[:in :inc] [:inc :out]] 
+                       [{:name :in
+                         :type :seq 
+                         :task-opts task-opts 
+                         :input (map (fn [n] {:n n}) (range n-messages))}
+                        {:name :inc
+                         :type :fn 
+                         :task-opts (assoc task-opts :onyx/fn ::my-inc)}
+                        {:name :out
+                         :type :async-out
+                         :chan-size 10000000
+                         :task-opts task-opts}]
+                       :onyx.task-scheduler/balanced)
+        output (run-test-job job 3)]
+    (is (= (set (map (fn [x] {:n (inc x)}) (range n-messages)))
+           (set (butlast (:out output)))))))
---
+++ test/onyx/test_boilerplate.clj
@@ -0,0 +1,36 @@
+(ns onyx.test-boilerplate
+  (:require [onyx.plugin.core-async :refer [take-segments!]]
+            [onyx.plugin.seq]
+            [onyx.job :refer [add-task]]
+            [onyx.tasks.core-async]
+            [onyx.tasks.seq]
+            [onyx.tasks.function]
+            [onyx.test-helper :refer [load-config with-test-env add-test-env-peers!]]))
+
+(defn build-job [workflow compact-job task-scheduler]
+  (reduce (fn [job {:keys [name task-opts type] :as task}]
+            (case type
+              :seq (add-task job (onyx.tasks.seq/input-serialized name task-opts (:input task)))
+              :fn (add-task job (onyx.tasks.function/function name task-opts))
+              :async-out (add-task job (onyx.tasks.core-async/output name task-opts (:chan-size task)))))
+          {:workflow workflow
+           :catalog []
+           :lifecycles []
+           :task-scheduler task-scheduler}
+          compact-job))
+
+(defn run-test-job [job n-peers]
+  (let [id (java.util.UUID/randomUUID)
+        config (load-config)
+        env-config (assoc (:env-config config) :onyx/tenancy-id id)
+        peer-config (assoc (:peer-config config) :onyx/tenancy-id id)]
+    (with-test-env [test-env [n-peers env-config peer-config]]
+      (let [{:keys [job-id]} (onyx.api/submit-job peer-config job)
+            _ (assert job-id "Job was not successfully submitted")
+            _ (onyx.test-helper/feedback-exception! peer-config job-id)
+            out-channels (onyx.plugin.core-async/get-core-async-channels job)]
+        (into {} 
+              (map (fn [[name chan]]
+                     [name (take-segments! chan)])
+                   out-channels))))))
+
