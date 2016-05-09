754c90145cc66797d3705424b9ecb328e871e328
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-05-09 23:14:26 +0800

    Onyx seq plugin

---
+++ src/onyx/plugin/seq.clj
@@ -0,0 +1,57 @@
+(ns onyx.plugin.seq
+  (:require [clojure.core.async :refer [poll! timeout chan alts!! >!! close!]]
+            [clojure.core.async.impl.protocols]
+            [clojure.set :refer [join]]
+            [taoensso.timbre :refer [fatal info debug] :as timbre]
+            [onyx.plugin.onyx-input :as i]
+            [onyx.plugin.onyx-output :as o]
+            [onyx.plugin.onyx-plugin :as p]))
+
+(defrecord AbsSeqReader [event completed? sequential rst segment offset]
+  p/OnyxPlugin
+
+  (start [this]
+    (let [sequential (:seq/seq event)] 
+      (assoc this :rst sequential :sequential sequential :offset -1)))
+
+  (stop [this event] 
+    (assoc this :rst nil :sequential nil))
+
+  i/OnyxInput
+
+  (checkpoint [this]
+    offset)
+
+  (recover [this checkpoint]
+    this)
+
+  (offset-id [this]
+    offset)
+
+  (segment [this]
+    segment)
+
+  (next-state [this event]
+    (let [segment (first rst)
+          remaining (rest rst)]
+      ;(println "Next state" segment remaining)
+      (assoc this
+             :segment segment
+             :rst remaining
+             :offset (if segment (inc offset) offset)
+             :completed? (empty? remaining))))
+
+  (segment-complete! [{:keys [conn]} segment])
+
+  (completed? [this]
+    (:completed? this)))
+
+(defn input [event]
+  (map->AbsSeqReader {:event event}))
+
+(defn inject-seq
+  [_ lifecycle]
+  {:seq/seq (:seq/sequential lifecycle)})
+
+(def in-calls
+  {:lifecycle/before-task-start inject-seq})
---
+++ src/onyx/tasks/seq.clj
@@ -0,0 +1,56 @@
+(ns onyx.tasks.seq
+  (:require [onyx.schema :as os]
+            [onyx.plugin.seq]
+            [schema.core :as s])
+  (:import [java.io FileReader BufferedReader]))
+
+(s/defn input-serialized
+  [task-name :- s/Keyword opts sequential]
+   {:task {:task-map (merge {:onyx/name task-name
+                             :onyx/plugin :onyx.plugin.seq/input
+                             :onyx/type :input
+                             :onyx/medium :seq
+                             :onyx/max-peers 1
+                             :onyx/doc "Reads segments from a seq"}
+                            opts)
+           :lifecycles [{:lifecycle/task task-name
+                         :seq/sequential sequential
+                         :lifecycle/calls :onyx.plugin.seq/in-calls}]}})
+
+(s/defn input-materialized
+  [task-name :- s/Keyword opts lifecycle]
+   {:task {:task-map (merge {:onyx/name task-name
+                             :onyx/plugin :onyx.plugin.seq/input
+                             :onyx/type :input
+                             :onyx/medium :seq
+                             :onyx/max-peers 1
+                             :onyx/doc "Reads segments from a seq"}
+                            opts)
+           :lifecycles [lifecycle]}})
+
+
+(defn inject-in-reader [event lifecycle]
+  (let [rdr (FileReader. (:buffered-reader/filename lifecycle))] 
+    {:seq/rdr rdr
+     :seq/seq (map (fn [line] {:line line}) 
+                   (line-seq (BufferedReader. rdr)))}))
+
+(defn close-reader [event lifecycle]
+  (.close (:seq/rdr event)))
+
+(def in-file-calls
+  {:lifecycle/before-task-start inject-in-reader
+   :lifecycle/after-task-stop close-reader})
+
+(s/defn input-file
+  [task-name :- s/Keyword opts filename]
+   {:task {:task-map (merge {:onyx/name task-name
+                             :onyx/plugin :onyx.plugin.seq/input
+                             :onyx/type :input
+                             :onyx/medium :seq
+                             :onyx/max-peers 1
+                             :onyx/doc "Reads segments from a seq"}
+                            opts)
+           :lifecycles [{:lifecycle/task :in
+                         :buffered-reader/filename filename
+                         :lifecycle/calls ::in-file-calls}]}})
