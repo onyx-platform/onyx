3d2b3125896d7731e864186e32036a5370ca404b
Michael Drogalis <mjd3089@rit.edu>
2016-05-30 12:40:44 -0700

    Adds a test to check that the replica cleans up after all groups leave.

--- test/onyx/log/generative_group_test.clj
+++ test/onyx/log/generative_group_test.clj
@@ -98,3 +98,41 @@
      (is (= (repeat (- n-groups 2) n-vpeers)
             (map count (vals (:groups-index replica)))))
      (is (= total-vpeers (count (keys (:groups-reverse-index replica))))))))
+
+(deftest all-groups-leave
+  (checking
+   "Checking clean slate when all groups join and leave"
+   (times 50)
+   [n-groups (gen/resize 20 gen/s-pos-int)
+    n-vpeers (gen/resize 40 gen/s-pos-int)
+    {:keys [replica log peer-choices]}
+    (let [gs
+          (for [g (range 1 (inc n-groups))]
+            (let [group-id (keyword (str "g" g))]
+              [(keyword (str "leave-" g))
+               {:predicate (fn [replica entry]
+                             (some #{group-id} (:groups replica)))
+                :queue [{:fn :group-leave-cluster
+                         :args {:id group-id}}]}]))
+          join-entries (log-gen/generate-join-queues
+                        (log-gen/generate-group-and-peer-ids n-groups n-vpeers))
+          entries (reduce (fn [result [group entries]]
+                            (assoc result group entries))
+                          join-entries
+                          gs)]
+      (log-gen/apply-entries-gen
+       (gen/return
+        {:replica {:job-scheduler :onyx.job-scheduler/greedy
+                   :messaging {:onyx.messaging/impl :dummy-messenger}}
+         :message-id 0
+         :entries entries
+         :log []
+         :peer-choices []})))]
+   (is (zero? (count (:groups replica))))
+   (is (zero? (count (:peers replica))))
+   (is (zero? (count (:aborted replica))))
+   (is (zero? (count (:orphaned-peers replica))))
+   (is (zero? (count (keys (:prepared replica)))))
+   (is (zero? (count (keys (:accepted replica)))))
+   (is (empty? (:groups-index replica)))
+   (is (empty? (:groups-reverse-index replica)))))
