10991c69cfeb50257e538bcef838a525ad36c031
Michael Drogalis <mjd3089@rit.edu>
2016-07-01 17:38:30 -0700

    Bump lein-unison to next version.

--- project.clj
+++ project.clj
@@ -43,7 +43,7 @@
                                   [joda-time/joda-time "2.8.2"]]
                    :plugins [[lein-jammin "0.1.1"]
                              [lein-set-version "0.4.1"]
-                             [mdrogalis/lein-unison "0.1.14"]
+                             [mdrogalis/lein-unison "0.1.15"]
                              [codox "0.8.8"]]}
              :reflection-check {:global-vars {*warn-on-reflection* true
                                               *assert* false
