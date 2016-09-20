d6fdbe4f1b290fe144bb192f48c79245b88622de
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-09-20 16:39:05 +0800

    Support :onyx/fn batches which return segments

--- src/onyx/peer/transform.clj
+++ src/onyx/peer/transform.clj
@@ -46,6 +46,38 @@
                  (transient (t/vector))
                  (transient (t/vector))))))
 
+(defn collect-next-segments-batch [f input]
+  (try (f input)
+       (catch Throwable e
+         (ex-info "Segments threw exception"
+                  {:exception e :segments input}))))
+
+(defn apply-fn-batch [f {:keys [onyx.core/batch] :as event}]
+  (let [batch-results (collect-next-segments f (map :message batch))] 
+    (when-not (= (count batch-results) (count batch))
+      (throw (ex-info ":onyx/batch? applied fns must return the same number of elements as the supplied batch size"
+                      {:elements batch-results
+                       :task (:onyx/name (:onyx.core/task-map event))})))
+    (assoc
+     event
+     :onyx.core/results
+     (->Results (doall
+                 (map
+                  (fn [leaf output]
+                    (let [segments (if (sequential? output) output (t/vector output))
+                          leaves (map (fn [message]
+                                        (-> leaf
+                                            (assoc :message message)
+                                            ;; not actually required, but it's safer
+                                            (assoc :ack-val nil)))
+                                      segments)]
+                      (->Result leaf leaves)))
+                  batch
+                  batch-results))
+                (transient (t/vector))
+                (transient (t/vector))
+                (transient (t/vector))))))
+
 (defn curry-params [f params]
   (reduce partial f params))
 
@@ -54,9 +86,12 @@
         bulk? (:bulk? compiled)
         g (curry-params f (:onyx.core/params event))
         rets
-        (if bulk?
-          (apply-fn-bulk g event)
-          (apply-fn-single g event))]
+        (cond (:bulk? compiled)
+              (apply-fn-bulk g event)
+              (:batch? compiled)
+              (apply-fn-batch g event)
+              :else
+              (apply-fn-single g event))]
     (tracef "[%s / %s] Applied fn to %s segments, returning %s new segments"
       (:onyx.core/id rets)
       (:onyx.core/lifecycle-id rets)
