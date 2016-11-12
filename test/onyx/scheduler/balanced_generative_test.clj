66c6b05eb302b8d88b9a291f8c8bb39e014e20dc
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-11-12 18:02:24 +0800

    Properly balance peer over multiple jobs Previously allocated jobs were acting in a greedy way. This commit fixes this behaviour and adds property testing that checks for appropriate spread

--- test/onyx/scheduler/balanced_generative_test.clj
+++ test/onyx/scheduler/balanced_generative_test.clj
@@ -8,13 +8,14 @@
             [onyx.test-helper :refer [job-allocation-counts]]
             [clojure.set :refer [intersection]]
             [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test :refer :all]
             [onyx.log.replica :as replica]
             [onyx.log.commands.common :as common]
             [com.gfredericks.test.chuck :refer [times]]
-            [com.gfredericks.test.chuck.clojure-test :refer [checking]]
+            [com.gfredericks.test.chuck.clojure-test :refer [checking for-all]]
             [taoensso.timbre :refer [info]]))
 
 (def onyx-id (java.util.UUID/randomUUID))
@@ -53,39 +54,48 @@
               :onyx/batch-size 20}]
    :task-scheduler :onyx.task-scheduler/balanced})
 
-(deftest combined-jobs
-  (let [n-jobs 4
-        n-peers 21
-        job-ids (repeatedly n-jobs #(java.util.UUID/randomUUID))
-        jobs-rets (map (fn [job-id]
-                         (api/create-submit-job-entry job-id
-                                                      peer-config
-                                                      job
-                                                      (planning/discover-tasks (:catalog job) (:workflow job))))
-                       job-ids)]
-    (checking
-     "Checking peers are allocated to job 1 even though job 2 is submitted and can't start."
-     (times 50)
-     [{:keys [replica log peer-choices]}
-      (log-gen/apply-entries-gen
-       (gen/return
-        {:replica base-replica 
-         :message-id 0
-         :entries (assoc (log-gen/generate-join-queues (log-gen/generate-group-and-peer-ids 1 n-peers))
-                         :jobs {:queue jobs-rets})
-         :log []
-         :peer-choices []}))]
-     (is (= 3 (count (:jobs replica))))
-     (println "Jobs" (:jobs replica))
-     (println "Allocatios" (:allocations replica))
-     #_(let [[t1 t2 t3] (:tasks (:args job-1-rets))
-           [t4 t5 t6] (:tasks (:args job-2-rets))]
-       (is (= 3 (count (get (get (:allocations replica) job-1-id) t1))))
-       (is (= 4 (count (get (get (:allocations replica) job-1-id) t2))))
-       (is (= 3 (count (get (get (:allocations replica) job-1-id) t3))))
-
-       (is (= 0 (count (get (get (:allocations replica) job-2-id) t4))))
-       (is (= 0 (count (get (get (:allocations replica) job-2-id) t5))))
-       (is (= 0 (count (get (get (:allocations replica) job-2-id) t6)))))
-     
-     )))
+(defspec allocate-as-many-jobs-as-possible {:num-tests (times 10)}
+  (let [;; TODO, make these parameters part of the property testing
+        min-peers-per-job 6]
+    (for-all [n-jobs (gen/such-that #(< % 10) gen/s-pos-int 1000)
+              n-jobs-to-allocate (gen/elements (range 0 (inc n-jobs))) 
+              ;; add excess peers, but not enough to allocate an extra job
+              n-excess (gen/elements (range 1 6))
+              n-peers (gen/return (+ n-excess (* min-peers-per-job n-jobs-to-allocate)))
+              job-ids (gen/vector gen/uuid n-jobs)
+              jobs-rets (gen/return (mapv (fn [job-id]
+                                            (api/create-submit-job-entry job-id
+                                                                         peer-config
+                                                                         job
+                                                                         (planning/discover-tasks (:catalog job) 
+                                                                                                  (:workflow job))))
+                                          job-ids))
+              {:keys [replica log peer-choices]} 
+              (log-gen/apply-entries-gen
+               (gen/return
+                {:replica base-replica 
+                 :message-id 0
+                 :entries (assoc (log-gen/generate-join-queues (log-gen/generate-group-and-peer-ids 1 n-peers))
+                                 :jobs {:queue jobs-rets})
+                 :log []
+                 :peer-choices []}))]
+             (is (= n-jobs-to-allocate 
+                    (count (:allocations replica))) "as many jobs are allocated as possible")
+             (let [allocation-counts (map (fn [job-id]
+                                            (reduce + (map (comp count val) 
+                                                           (get-in replica [:allocations job-id]))))
+                                          job-ids)]
+               (let [sorted-cnts (-> allocation-counts
+                                     set
+                                     (disj 0)
+                                     vec
+                                     sort)]
+                 (is (or (< (count sorted-cnts) 2)
+                         ;; at max two possible counts can exist because everything should be balanced
+                         ;; e.g. 7 on one job, and 8 on two jobs
+                         (and (= (count sorted-cnts) 2)
+                              (= (inc (first sorted-cnts)) (second sorted-cnts))))))
+               (is (every? (fn [cnt]
+                             (or (zero? cnt) (>= cnt 6)))
+                           allocation-counts)
+                   ["no partially allocated jobs" (:allocations replica)])))))
