881a04dffc00c4cef01a6fd6aa53d92d034638e4
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-11-15 15:16:21 +0800

    Remove unused keys from messaging group

--- src/onyx/messaging/aeron/messaging_group.clj
+++ src/onyx/messaging/aeron/messaging_group.clj
@@ -10,25 +10,17 @@
   component/Lifecycle
   (start [component]
     (taoensso.timbre/info "Starting Aeron Peer Group")
-    (let [;bind-addr (common/bind-addr peer-config)
-          ;external-addr (common/external-addr peer-config)
-          ;port (:onyx.messaging/peer-port peer-config)
-          ticket-counters (atom {})
+    (let [ticket-counters (atom {})
           embedded-media-driver (component/start (md/->EmbeddedMediaDriver peer-config))]
       (assoc component
-             ;:bind-addr bind-addr
-             ;:external-addr external-addr
              :ticket-counters ticket-counters
-             :embedded-media-driver embedded-media-driver
-             ;:port port
-             )))
+             :embedded-media-driver embedded-media-driver)))
 
   (stop [{:keys [embedded-media-driver] :as component}]
     (taoensso.timbre/info "Stopping Aeron Peer Group")
     (component/stop embedded-media-driver)
-    (assoc component :embedded-media-driver nil 
-           ;:bind-addr nil 
-           ;:external-addr nil :external-channel nil 
+    (assoc component 
+           :embedded-media-driver nil 
            :ticket-counters nil)))
 
 (defmethod m/build-messenger-group :aeron [peer-config]
