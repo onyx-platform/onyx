afdca6bed97c0232e3ffbc6fab29a31c59d5605a
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-02-16 21:50:56 -0800

    Update dependencies.

--- project.clj
+++ project.clj
@@ -16,18 +16,18 @@
                  [org.apache.curator/curator-framework "2.9.1"]
                  [org.apache.curator/curator-test "2.9.1"]
                  [org.apache.zookeeper/zookeeper "3.4.6" :exclusions [org.slf4j/slf4j-log4j12]]
-                 [net.cgrand/xforms "0.8.2"]
+                 [net.cgrand/xforms "0.9.0"]
                  [org.slf4j/slf4j-api "1.7.12"]
                  [org.slf4j/slf4j-nop "1.7.12"]
                  [org.btrplace/scheduler-api "0.46"]
                  [org.btrplace/scheduler-choco "0.46"]
                  [com.stuartsierra/dependency "0.2.0"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [metrics-clojure "2.8.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [metrics-clojure "2.9.0"]
                  [com.taoensso/timbre "4.8.0"]
-                 [com.taoensso/nippy "2.12.2"]
+                 [com.taoensso/nippy "2.13.0"]
                  [io.aeron/aeron-all "1.1.0"]
-                 [io.replikativ/hasch "0.3.2" 
+                 [io.replikativ/hasch "0.3.3" 
                   :exclusions [org.clojure/clojurescript com.cognitect/transit-clj 
                                com.cognitect/transit-cljs org.clojure/data.fressian 
                                com.cemerick/austin]]
