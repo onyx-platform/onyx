85da340a44d7c4c537d54c73a09af7cf94a344bf
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-08-31 00:15:46 -0700

    Make a new test.

--- test/onyx/windowing/aggregation_cep_test.clj
+++ test/onyx/windowing/aggregation_cep_test.clj
@@ -1,36 +1,54 @@
-(ns onyx.windowing.aggregation-max-test
+(ns onyx.windowing.aggregation-cep-test
   (:require [clojure.core.async :refer [chan >!! <!! close! sliding-buffer]]
             [clojure.test :refer [deftest is]]
             [onyx.plugin.core-async :refer [take-segments!]]
             [onyx.static.uuid :refer [random-uuid]]
             [onyx.test-helper :refer [load-config with-test-env]]
+            [onyx-cep.api :as cep]
+            [onyx-cep.runtime :as rt]
             [onyx.api]))
 
 (def input
-  [{:id 1  :age 21 :event-time #inst "2015-09-13T03:00:00.829-00:00"}
-   {:id 2  :age 12 :event-time #inst "2015-09-13T03:04:00.829-00:00"}
-   {:id 3  :age 3  :event-time #inst "2015-09-13T03:05:00.829-00:00"}
-   {:id 4  :age 64 :event-time #inst "2015-09-13T03:06:00.829-00:00"}
-   {:id 5  :age 53 :event-time #inst "2015-09-13T03:07:00.829-00:00"}
-   {:id 6  :age 52 :event-time #inst "2015-09-13T03:08:00.829-00:00"}
-   {:id 7  :age 24 :event-time #inst "2015-09-13T03:09:00.829-00:00"}
-   {:id 8  :age 35 :event-time #inst "2015-09-13T03:15:00.829-00:00"}
-   {:id 9  :age 49 :event-time #inst "2015-09-13T03:25:00.829-00:00"}
-   {:id 10 :age 37 :event-time #inst "2015-09-13T03:45:00.829-00:00"}
-   {:id 11 :age 15 :event-time #inst "2015-09-13T03:03:00.829-00:00"}
-   {:id 12 :age 22 :event-time #inst "2015-09-13T03:56:00.829-00:00"}
-   {:id 13 :age 83 :event-time #inst "2015-09-13T03:59:00.829-00:00"}
-   {:id 14 :age 60 :event-time #inst "2015-09-13T03:32:00.829-00:00"}
-   {:id 15 :age 35 :event-time #inst "2015-09-13T03:16:00.829-00:00"}])
+  [{:id 1  :age 3 :event-time #inst "2015-09-13T03:00:00.829-00:00"}
+   {:id 2  :age 6 :event-time #inst "2015-09-13T03:04:00.829-00:00"}
+   {:id 3  :age 2 :event-time #inst "2015-09-13T03:05:00.829-00:00"}
+   {:id 4  :age 7 :event-time #inst "2015-09-13T03:06:00.829-00:00"}
+   {:id 5  :age 10 :event-time #inst "2015-09-13T03:07:00.829-00:00"}
+   {:id 6  :age 11 :event-time #inst "2015-09-13T03:08:00.829-00:00"}
+   {:id 7  :age 19 :event-time #inst "2015-09-13T03:09:00.829-00:00"}
+   {:id 8  :age 5 :event-time #inst "2015-09-13T03:15:00.829-00:00"}])
 
 (def expected-windows
-  [[Double/NEGATIVE_INFINITY Double/POSITIVE_INFINITY 83]])
+  '[[(7 10) (10 11) (11 19)]])
 
 (def test-state (atom []))
 
-(defn update-atom! [event window trigger {:keys [lower-bound upper-bound event-type] :as opts} extent-state]
-  (when-not (= :job-completed event-type)
-    (swap! test-state conj [lower-bound upper-bound extent-state])))
+(defn aggregation-apply-log [window runtime v]
+  (rt/evaluate-segment runtime v))
+
+(defn aggregation-fn-init [window]
+  (-> (cep/new-pattern-sequence "test")
+      (cep/begin "start" (fn [event & context] (> event 5)))
+      (cep/next "bigger" (fn [event & context] (> event 9)))
+      (rt/initialize-runtime)))
+
+(defn aggregation-fn [window segment]
+  (:age segment))
+
+(defn super-aggregation [window state-1 state-2]
+  (throw (Exception.)))
+
+(def ^:export cep
+  {:aggregation/init aggregation-fn-init
+   :aggregation/create-state-update aggregation-fn
+   :aggregation/apply-state-update aggregation-apply-log
+   :aggregation/super-aggregation-fn super-aggregation})
+
+(defn update-atom! [event window trigger 
+                    {:keys [lower-bound upper-bound event-type] :as opts} 
+                    extent-state]
+  (when-not (empty? (:matches extent-state)) 
+    (swap! test-state conj (:matches extent-state))))
 
 (def in-chan (atom nil))
 (def in-buffer (atom nil))
@@ -86,16 +104,19 @@
         [{:window/id :collect-segments
           :window/task :identity
           :window/type :global
-          :window/aggregation [:onyx.windowing.aggregation/max :age]
-          :window/window-key :event-time
+          ;; TODO, need incremental because we need to know the extents in the extent store
+          ;; This can change if we just store the extents, not the final vlaues.
+          ;; Suggest a third window/materialize scheme?
+          :window/materialize [:lazy]
+          :window/aggregation [::cep :age]
           :window/init 0}]
 
         triggers
         [{:trigger/window-id :collect-segments
+          :trigger/on :onyx.triggers/timer
+          :trigger/period [1 :seconds]
           :trigger/id :sync
-          
-          :trigger/on :onyx.triggers/segment
-          :trigger/threshold [15 :elements]
+          :trigger/post-evictor [:all]
           :trigger/sync ::update-atom!}]
 
         lifecycles
@@ -112,7 +133,6 @@
     (with-test-env [test-env [3 env-config peer-config]]
       (doseq [i input]
         (>!! @in-chan i))
-      (close! @in-chan)
       (let [{:keys [job-id]} (onyx.api/submit-job
                               peer-config
                               {:catalog catalog
@@ -121,7 +141,10 @@
                                :windows windows
                                :triggers triggers
                                :task-scheduler :onyx.task-scheduler/balanced})
+            _ (Thread/sleep 5000)
+            _ (close! @in-chan)
             _ (onyx.test-helper/feedback-exception! peer-config job-id)
             results (take-segments! @out-chan 50)]
+
         (is (= (into #{} input) (into #{} results)))
         (is (= expected-windows @test-state))))))
