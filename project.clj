ca35ccca429a55cf942eb4eace39610f4de18d09
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-09-13 11:17:12 -0700

    Add more projects to the build matrix.

--- project.clj
+++ project.clj
@@ -184,17 +184,17 @@
      :release-script "build-site.sh"
      :skip-compatibility? true
      :merge "master"}
-    #_{:git "git@onyx-local-rt:onyx-platform/onyx-local-rt.git"
+    {:git "git@onyx-local-rt:onyx-platform/onyx-local-rt.git"
      :branch "compatibility"
      :release-branch "master"
      :release-script "scripts/release.sh"
      :merge "master"}
-    #_{:git "git@onyx-spec:onyx-platform/onyx-spec.git"
+    {:git "git@onyx-spec:onyx-platform/onyx-spec.git"
      :branch "compatibility"
      :release-branch "master"
      :release-script "scripts/release.sh"
      :merge "master"}
-    #_{:git "git@lib-onyx:onyx-platform/lib-onyx.git"
+    {:git "git@lib-onyx:onyx-platform/lib-onyx.git"
      :branch "compatibility"
      :release-branch "master"
      :release-script "scripts/release.sh"
