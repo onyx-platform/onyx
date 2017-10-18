c9bead98c99615de6044f851f6b433ab5e2df432
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-10-17 23:12:56 -0700

    flow-conditions exceptions handling will restrict flow of regular segments to error task.

--- test/onyx/peer/flow_exception_test.clj
+++ test/onyx/peer/flow_exception_test.clj
@@ -11,6 +11,7 @@
 (def in-buffer-1 (atom nil))
 
 (def out-chan (atom nil))
+(def error-out-chan (atom nil))
 
 (defn inject-in-ch [event lifecycle]
   {:core.async/buffer in-buffer-1
@@ -19,11 +20,17 @@
 (defn inject-out-ch [event lifecycle]
   {:core.async/chan @out-chan})
 
+(defn inject-error-out-ch [event lifecycle]
+  {:core.async/chan @error-out-chan})
+
 (def in-calls
   {:lifecycle/before-task-start inject-in-ch})
 
 (def out-calls
   {:lifecycle/before-task-start inject-out-ch})
+
+(def error-out-calls
+  {:lifecycle/before-task-start inject-error-out-ch})
 
 (defn even-exception? [event old e all-new]
   (= (:error (ex-data e)) :even))
@@ -43,6 +50,8 @@
         (= 5 n)
         (throw (ex-info "Number was 5" {:error :five :n n}))
         :else segment))
+
+(def always-true? (constantly true))
 
 (deftest flow-exceptions
   (let [id (random-uuid)
@@ -70,25 +79,36 @@
                   :onyx/medium :core.async
                   :onyx/batch-size batch-size
                   :onyx/max-peers 1
+                  :onyx/doc "Writes segments to a core.async channel"}
+
+                 {:onyx/name :error-out
+                  :onyx/plugin :onyx.plugin.core-async/output
+                  :onyx/type :output
+                  :onyx/medium :core.async
+                  :onyx/batch-size batch-size
+                  :onyx/max-peers 1
                   :onyx/doc "Writes segments to a core.async channel"}]
 
         workflow [[:in :inc]
+                  [:inc :error-out]
                   [:inc :out]]
 
         lifecycles [{:lifecycle/task :in
                      :lifecycle/calls :onyx.peer.flow-exception-test/in-calls}
                     {:lifecycle/task :out
-                     :lifecycle/calls :onyx.peer.flow-exception-test/out-calls}]
+                     :lifecycle/calls :onyx.peer.flow-exception-test/out-calls}
+                    {:lifecycle/task :error-out
+                     :lifecycle/calls :onyx.peer.flow-exception-test/error-out-calls}]
 
         flow-conditions [{:flow/from :inc
-                          :flow/to [:out]
+                          :flow/to [:error-out]
                           :flow/short-circuit? true
                           :flow/thrown-exception? true
                           :flow/predicate [:onyx.peer.flow-exception-test/even-exception?]
                           :flow/post-transform :onyx.peer.flow-exception-test/transform-even}
 
                          {:flow/from :inc
-                          :flow/to [:out]
+                          :flow/to [:error-out]
                           :flow/short-circuit? true
                           :flow/thrown-exception? true
                           :flow/predicate [:onyx.peer.flow-exception-test/five-exception?]
@@ -97,8 +117,9 @@
     (reset! in-chan (chan 100))
     (reset! in-buffer-1 {})
     (reset! out-chan (chan (sliding-buffer 100)))
+    (reset! error-out-chan (chan (sliding-buffer 100)))
 
-    (with-test-env [test-env [3 env-config peer-config]]
+    (with-test-env [test-env [4 env-config peer-config]]
       (doseq [x (range 20)]
         (>!! @in-chan {:n x}))
       (close! @in-chan)
@@ -110,25 +131,27 @@
            (onyx.api/submit-job peer-config)
            (:job-id)
            (onyx.test-helper/feedback-exception! peer-config))
-      (let [results (take-segments! @out-chan 50)]
-        (is (= #{{:error? true :value 0}
-                 {:n 1}
-                 {:error? true :value 2}
-                 {:n 3}
-                 {:error? true :value 4}
-                 {:error? true :value "abc"}
-                 {:error? true :value 6}
-                 {:n 7}
-                 {:error? true :value 8}
-                 {:n 9}
-                 {:error? true :value 10}
-                 {:n 11}
-                 {:error? true :value 12}
-                 {:n 13}
-                 {:error? true :value 14}
-                 {:n 15}
-                 {:error? true :value 16}
-                 {:n 17}
-                 {:error? true :value 18}
-                 {:n 19}}
-               (into #{} results)))))))
+      (let [out-results (take-segments! @out-chan 50)
+            error-out-results (take-segments! @error-out-chan 50)]
+        (is (= [{:n 1}
+                {:n 3}
+                {:n 7}
+                {:n 9}
+                {:n 11}
+                {:n 13}
+                {:n 15}
+                {:n 17}
+                {:n 19}]
+               out-results))
+        (is (= [{:error? true :value 0}
+                {:error? true :value 2}
+                {:error? true :value 4}
+                {:error? true :value "abc"}
+                {:error? true :value 6}
+                {:error? true :value 8}
+                {:error? true :value 10}
+                {:error? true :value 12}
+                {:error? true :value 14}
+                {:error? true :value 16}
+                {:error? true :value 18}]
+               error-out-results))))))
