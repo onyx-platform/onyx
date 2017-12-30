6afae4042eda7422aab8c3a2798545960fa93a4e
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-12-30 15:10:54 -0800

    Release script broken on later git.

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,7 +13,7 @@ else
   git checkout master
 
   git remote update
-  git status -uno|grep up-to-date
+  git status -uno|grep up.to.date
 
   if [[ $? != 0 ]]; then
 	  echo "master is not up to date with remote. Please pull / merge with master first"
