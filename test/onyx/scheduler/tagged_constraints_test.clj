81d41e58dc2d5870a7939bb68a4e0daa103310e6
khoda <elizavetaskull@gmail.com>
2018-01-04 13:34:40 +0700

    no-op-node for task-tagged-constraints

--- test/onyx/scheduler/tagged_constraints_test.clj
+++ test/onyx/scheduler/tagged_constraints_test.clj
@@ -3,6 +3,7 @@
             [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
+            [clojure.data :refer [diff]]
             [com.gfredericks.test.chuck :refer [times]]
             [com.gfredericks.test.chuck.clojure-test :refer [checking]]
             [onyx.scheduling.common-job-scheduler :refer [reconfigure-cluster-workload]]
@@ -11,167 +12,235 @@
             [onyx.static.planning :as planning]
             [onyx.api]))
 
+(def old-replica {:allocations {}})
+
+(deftest diff-replicas-after-reconfigured
+  (let [new-replica (one-group
+                     {:jobs [:j1]
+                      :allocations {}
+                      :peers [:p1 :p2 :p3]
+                      :peer-state {:p1 :idle :p2 :idle :p3 :idle}
+                      :tasks {:j1 [:t1 :t2 :t3]}
+                      :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                                        :j2 :onyx.task-scheduler/balanced}
+                      :job-scheduler :onyx.job-scheduler/balanced
+                      :required-tags {:j1 {:t1 [:datomic]
+                                           :t2 [:datomic]
+                                           :t3 [:datomic]}}
+                      :peer-tags {:p1 [:datomic]
+                                  :p2 [:datomic]
+                                  :p3 [:datomic]}
+                      :messaging {:onyx.messaging/impl :aeron}})
+        reconfigured (reconfigure-cluster-workload new-replica old-replica)]
+    (is
+      (=  '({:allocations nil}
+            {:allocations {:j1 {:t2 [:p1]
+                                :t3 [:p2]
+                                :t1 [:p3]}}
+             :peer-sites {:p1 {} :p2 {} :p3 {}}
+             :task-slot-ids {:j1 {:t2 {:p1 0}
+                                  :t3 {:p2 0}
+                                  :t1 {:p3 0}}}
+             :allocation-version {:j1 nil}
+             :coordinators {:j1 :p1}
+             :message-short-ids {}})
+         (butlast (diff new-replica reconfigured))))))
+
+(deftest peers-allocated-with-tags-if-peers-more-than-necessary
+  (is
+   (= {:j1 {:t1 [:p1]
+            :t2 [:p6]
+            :t3 [:p2]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3 :p4 :p5 :p6]
+          :peer-state {:p1 :idle :p2 :idle :p3 :idle :p4 :idle :p5 :idle :p6 :idle}
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :min-required-peers {:j1 {:t1 1 :t2 1 :t3 1}},
+          :saturation {:j1 3}
+          :task-saturation {:j1 {:t1 1 :t2 1 :t3 1}}
+          :required-tags {:j1 {:t1 [:s3]
+                               :t2 [:datomic]
+                               :t3 [:datomic]}}
+          :peer-tags {:p1 [:s3] :p2 [:datomic] :p3 [:datomic] :p4 [:s3] :p5 [:s3] :p6 [:datomic]}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
+
 ;; Following tests are broken because reconfigure-cluster-workload
 ;; now takes an old and a new replica
 
-; (deftest no-peers-are-allocated-missing-tags
-;   (is
-;    (= {}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1]
-;           :allocations {}
-;           :peers [:p1 :p2 :p3]
-;           :peer-state {:p1 :idle :p2 :idle :p3 :idle}
-;           :tasks {:j1 [:t1 :t2 :t3]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :required-tags {:j1 {:t1 [:datomic]
-;                                :t2 [:datomic]
-;                                :t3 [:datomic]}}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
+(deftest no-peers-are-allocated-missing-tags
+  (is
+   (= {}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3]
+          :peer-state {:p1 :idle :p2 :idle :p3 :idle}
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :required-tags {:j1 {:t1 [:datomic]
+                               :t2 [:datomic]
+                               :t3 [:datomic]}}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
 
-; (deftest peers-allocated-with-tags
-;   (is
-;    (= {:j1 {:t1 [:p3]
-;             :t2 [:p2]
-;             :t3 [:p1]}}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1]
-;           :allocations {}
-;           :peers [:p1 :p2 :p3]
-;           :tasks {:j1 [:t1 :t2 :t3]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :required-tags {:j1 {:t1 [:datomic]
-;                                :t2 [:datomic]
-;                                :t3 [:datomic]}}
-;           :peer-tags {:p1 [:datomic] :p2 [:datomic] :p3 [:datomic]}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
 
-; (deftest only-tagged-peers-allocated
-;   (is
-;    (= {:j1 {:t1 [:p4]
-;             :t2 [:p2]
-;             :t3 [:p1]}}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1]
-;           :allocations {}
-;           :peers [:p1 :p2 :p3 :p4]
-;           :tasks {:j1 [:t1 :t2 :t3]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :required-tags {:j1 {:t1 [:datomic]
-;                                :t2 [:datomic]
-;                                :t3 [:datomic]}}
-;           :peer-tags {:p1 [:datomic]
-;                       :p2 [:datomic]
-;                       :p3 []
-;                       :p4 [:datomic]}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
+(deftest peers-allocated-with-tags
+  (is
+   (= {:j1 {:t1 [:p3]
+            :t2 [:p1]
+            :t3 [:p2]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3]
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :required-tags {:j1 {:t1 [:datomic]
+                               :t2 [:datomic]
+                               :t3 [:datomic]}}
+          :peer-tags {:p1 [:datomic] :p2 [:datomic] :p3 [:datomic]}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
 
-; (deftest one-task-tagged
-;   (is
-;    (= {:j1 {:t1 [:p1]
-;             :t2 [:p3]
-;             :t3 [:p2]}}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1]
-;           :allocations {}
-;           :peers [:p1 :p2 :p3]
-;           :tasks {:j1 [:t1 :t2 :t3]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :required-tags {:j1 {:t1 [:datomic]}}
-;           :peer-tags {:p1 [:datomic]}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
+(deftest only-tagged-peers-allocated
+  (is
+   (= {:j1 {:t1 [:p4]
+            :t2 [:p1]
+            :t3 [:p2]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3 :p4]
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :required-tags {:j1 {:t1 [:datomic]
+                               :t2 [:datomic]
+                               :t3 [:datomic]}}
+          :peer-tags {:p1 [:datomic]
+                      :p2 [:datomic]
+                      :p3 []
+                      :p4 [:datomic]}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
 
-; (deftest one-task-tagged-max-peers
-;   (is
-;    (= {:j1 {:t1 [:p1]
-;             :t2 [:p3 :p4]
-;             :t3 [:p2]}}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1]
-;           :allocations {}
-;           :peers [:p1 :p2 :p3 :p4]
-;           :tasks {:j1 [:t1 :t2 :t3]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :required-tags {:j1 {:t1 [:datomic]}}
-;           :peer-tags {:p1 [:datomic]}
-;           :task-saturation {:j1 {:t1 1}}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
+(deftest one-task-tagged
+  (is
+   (= {:j1 {:t1 [:p1]
+            :t2 [:p3]
+            :t3 [:p2]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3]
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :required-tags {:j1 {:t1 [:datomic]}}
+          :peer-tags {:p1 [:datomic]}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
 
-; (deftest two-tags
-;   (is
-;    (= {:j1 {:t1 [:p1]
-;             :t2 [:p2]
-;             :t3 [:p3]}}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1]
-;           :allocations {}
-;           :peers [:p1 :p2 :p3]
-;           :tasks {:j1 [:t1 :t2 :t3]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :required-tags {:j1 {:t1 []
-;                                :t2 [:mysql :datomic]
-;                                :t3 [:datomic]}}
-;           :peer-tags {:p1 []
-;                       :p2 [:datomic :mysql]
-;                       :p3 [:datomic]}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
+(deftest one-task-tagged-max-peers
+  (is
+   (= {:j1 {:t1 [:p1]
+            :t2 [:p3 :p4]
+            :t3 [:p2]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3 :p4]
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :required-tags {:j1 {:t1 [:datomic]}}
+          :peer-tags {:p1 [:datomic]}
+          :task-saturation {:j1 {:t1 1}}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
 
-; (deftest two-jobs
-;   (is
-;    (= {:j1 {:t1 [:p7]
-;             :t2 [:p3]
-;             :t3 [:p5]}
-;        :j2 {:t4 [:p8]
-;             :t5 [:p4]
-;             :t6 [:p1]}}
-;       (:allocations
-;        (reconfigure-cluster-workload
-;         (one-group
-;          {:jobs [:j1 :j2]
-;           :allocations {:j1 {:t1 [:p7]
-;                              :t2 [:p3 :p4 :p5]
-;                              :t3 [:p8]}}
-;           :peers [:p1 :p3 :p4 :p5 :p7 :p8]
-;           :tasks {:j1 [:t1 :t2 :t3]
-;                   :j2 [:t4 :t5 :t6]}
-;           :task-schedulers {:j1 :onyx.task-scheduler/balanced
-;                             :j2 :onyx.task-scheduler/balanced}
-;           :job-scheduler :onyx.job-scheduler/balanced
-;           :task-saturation {:j1 {:t1 1 :t2 42 :t3 1}
-;                             :t2 {:t4 1 :t5 42 :t6 1}}
-;           :required-tags {:j1 {:t1 [:datomic]
-;                                :t2 []
-;                                :t3 []}
-;                           :j2 {:t4 [:datomic]
-;                                :t5 []
-;                                :t6 []}}
-;           :peer-tags {:p7 [:datomic]
-;                       :p8 [:datomic]}
-;           :messaging {:onyx.messaging/impl :aeron}}))))))
+(deftest two-tags
+  (is
+   (= {:j1 {:t1 [:p1]
+            :t2 [:p2]
+            :t3 [:p3]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1]
+          :allocations {}
+          :peers [:p1 :p2 :p3]
+          :tasks {:j1 [:t1 :t2 :t3]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :required-tags {:j1 {:t1 []
+                               :t2 [:mysql :datomic]
+                               :t3 [:datomic]}}
+          :peer-tags {:p1 []
+                      :p2 [:datomic :mysql]
+                      :p3 [:datomic]}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
+
+(deftest two-jobs
+  (is
+   (= {:j1 {:t1 [:p7]
+            :t2 [:p3]
+            :t3 [:p5]}
+       :j2 {:t4 [:p8]
+            :t5 [:p4]
+            :t6 [:p1]}}
+      (:allocations
+       (reconfigure-cluster-workload
+        (one-group
+         {:jobs [:j1 :j2]
+          :allocations {:j1 {:t1 [:p7]
+                             :t2 [:p3 :p4 :p5]
+                             :t3 [:p8]}}
+          :peers [:p1 :p3 :p4 :p5 :p7 :p8]
+          :tasks {:j1 [:t1 :t2 :t3]
+                  :j2 [:t4 :t5 :t6]}
+          :task-schedulers {:j1 :onyx.task-scheduler/balanced
+                            :j2 :onyx.task-scheduler/balanced}
+          :job-scheduler :onyx.job-scheduler/balanced
+          :task-saturation {:j1 {:t1 1 :t2 42 :t3 1}
+                            :t2 {:t4 1 :t5 42 :t6 1}}
+          :required-tags {:j1 {:t1 [:datomic]
+                               :t2 []
+                               :t3 []}
+                          :j2 {:t4 [:datomic]
+                               :t5 []
+                               :t6 []}}
+          :peer-tags {:p7 [:datomic]
+                      :p8 [:datomic]}
+          :messaging {:onyx.messaging/impl :aeron}})
+        old-replica)))))
 
 ; (def onyx-id "tagged-gen-test-id")
 
@@ -274,7 +343,7 @@
 ;       (is (= #{[:g2-p9 [:special-peer]] [:g2-p10 [:special-peer]]} (set (remove (comp nil? val) (:peer-tags replica)))))
 ;       (is (= 8 (count (:peers replica))))
 ;       (is (= [4 4]
-;              (map (comp (partial apply +) vals) 
+;              (map (comp (partial apply +) vals)
 ;                   (get-counts replica
 ;                               [{:job-id job-1-id}
 ;                                {:job-id job-2-id}]))))
@@ -310,7 +379,7 @@
 ;                       job-1-id
 ;                       peer-config
 ;                       job-1
-;                       (planning/discover-tasks (:catalog job-1) (:workflow job-1)))] 
+;                       (planning/discover-tasks (:catalog job-1) (:workflow job-1)))]
 ;     (checking
 ;       "Tagged peer leaves, deallocates the job requiring that peer"
 ;       (times 50)
@@ -332,7 +401,7 @@
 ;       (is (= #{} (set (remove (comp nil? val) (:peer-tags replica)))))
 ;         (is (= 3 (count (:peers replica))))
 ;         (is (= [0]
-;                (map (comp (partial apply +) vals) 
+;                (map (comp (partial apply +) vals)
 ;                     (get-counts replica
 ;                                 [{:job-id job-1-id}])))))))
 
@@ -397,7 +466,7 @@
 ;                       job-3-id
 ;                       peer-config
 ;                       job-3
-;                       (planning/discover-tasks (:catalog job-3) (:workflow job-3)))] 
+;                       (planning/discover-tasks (:catalog job-3) (:workflow job-3)))]
 ;     (checking
 ;       "More peers than necessary are tagged, job is killed, still has balanced allocation"
 ;       (times 50)
@@ -412,7 +481,7 @@
 ;                           (log-gen/generate-join-queues (log-gen/generate-group-and-peer-ids 2 1 11 10)))
 ;                    :job-1 {:queue [job-entry-1]}
 ;                    :job-2 {:queue [job-entry-2]}
-;                    :job-3 {:queue [job-entry-3 
+;                    :job-3 {:queue [job-entry-3
 ;                                    {:fn :kill-job :args {:job job-3-id}}]}
 ;                    :leave-untagged {:predicate (fn [replica entry]
 ;                                                  (some #{:g2-p14} (:peers replica)))
