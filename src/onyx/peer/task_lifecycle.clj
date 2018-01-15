486e7064e4e4499ce92c1ed4fb96fea96c5fb643
Lucas Bradstreet <lucasbradstreet@gmail.com>
2018-01-14 23:09:02 -0800

    Fix max-segments-per-barrier functionality.

--- src/onyx/peer/task_lifecycle.clj
+++ src/onyx/peer/task_lifecycle.clj
@@ -637,7 +637,7 @@
 (defn build-read-batch [{:keys [onyx.core/task-map onyx.core/since-barrier-count] :as event}] 
   (if (source-task? event) 
     (let [batch-size (long (:onyx/batch-size task-map))
-          max-segments-per-barrier (long (get :onyx/max-segments-per-barrier task-map Long/MAX_VALUE))
+          max-segments-per-barrier (long (get task-map :onyx/max-segments-per-barrier Long/MAX_VALUE))
           batch-timeout (long (ms->ns (arg-or-default :onyx/batch-timeout task-map)))
           apply-watermark-fn (build-apply-watermark-fn event)]
       (fn [state]
