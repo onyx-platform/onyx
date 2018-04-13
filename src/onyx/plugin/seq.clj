a33cccc2ba19a9b330e9f4005f708440c49cf23a
Lucas Bradstreet <lucasbradstreet@gmail.com>
2018-04-13 10:44:20 -0700

    Remove unused require.

--- src/onyx/plugin/seq.clj
+++ src/onyx/plugin/seq.clj
@@ -1,6 +1,5 @@
 (ns onyx.plugin.seq
-  (:require [clojure.core.async :refer [poll! timeout chan alts!! >!! close!]]
-            [clojure.core.async.impl.protocols]
+  (:require [clojure.core.async.impl.protocols]
             [clojure.set :refer [join]]
             [onyx.plugin.protocols :as p]
             [taoensso.timbre :refer [fatal info debug] :as timbre]))
